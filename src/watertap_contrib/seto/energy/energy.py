b03efcd4693eed52a22922275327fe2a3c9e6101
kurbansitterley <kurban.sitterley@nrel.gov>
2022-11-16 16:32:27 -0700

    add energy folder (for future development)

---
+++ src/watertap_contrib/seto/energy/energy.py
@@ -0,0 +1,24 @@
+
+"""
+This module contains common methods for determining the energy consumption or 
+production for SETO modules.
+"""
+
+import idaes.logger as idaeslog
+
+from pyomo.environ import Param, Var, units as pyunits
+
+import PySAM.Pvsamv1 as pv
+import PySAM.Grid as grid
+import PySAM.Utilityrate5 as utilityrate
+import PySAM.Singleowner as singleowner
+
+import json
+import numpy as np
+
+# Some more inforation about this module
+__author__ = "Kurban Sitterley"
+
+# Set up logger
+_log = idaeslog.getLogger(__name__)
+
