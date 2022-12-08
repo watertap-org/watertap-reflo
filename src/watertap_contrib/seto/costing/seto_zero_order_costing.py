d8cb0641029638c99a0727e3e6891bc395b326ba
kurbansitterley <kurban.sitterley@nrel.gov>
2022-12-08 16:36:20 -0700

    costing pkg edits

--- src/watertap_contrib/seto/costing/seto_zero_order_costing.py
+++ src/watertap_contrib/seto/costing/seto_zero_order_costing.py
@@ -39,17 +39,121 @@ class SETOZeroOrderCostingData(ZeroOrderCostingData):
             doc="Capital cost of solar energy generation system",
         )
 
-        blk.installed_cost = pyo.Var(
+        blk.direct_cost = pyo.Var(
             initialize=0,
             units=blk.config.flowsheet_costing_block.base_currency,
             bounds=(0, None),
-            doc="Capital cost of solar energy generation system",
+            doc="Direct costs of PV system",
         )
 
-        blk.installed_cost.fix()
+        blk.indirect_cost = pyo.Var(
+            initialize=0,
+            units=blk.config.flowsheet_costing_block.base_currency,
+            bounds=(0, None),
+            doc="Indirect costs of PV system",
+        )
+
+        blk.sales_tax = pyo.Var(
+            initialize=0,
+            units=blk.config.flowsheet_costing_block.base_currency,
+            bounds=(0, None),
+            doc="Sales tax for PV system",
+        )
+
+        blk.system_capacity = pyo.Var(
+            initialize=0,
+            units=pyo.units.watt,
+            bounds=(0, None),
+            doc="DC system capacity for PV system",
+        )
+
+        blk.cost_per_watt_module = pyo.Var(
+            initialize=0.41,
+            units=blk.config.flowsheet_costing_block.base_currency / pyo.units.watt,
+            bounds=(0, None),
+            doc="Cost per watt for solar module",
+        )
+
+        blk.cost_per_watt_inverter = pyo.Var(
+            initialize=0.05,
+            units=blk.config.flowsheet_costing_block.base_currency / pyo.units.watt,
+            bounds=(0, None),
+            doc="Cost per watt for inverter",
+        )
+
+        blk.cost_per_watt_other = pyo.Var(
+            initialize=0.1,
+            units=blk.config.flowsheet_costing_block.base_currency / pyo.units.watt,
+            bounds=(0, None),
+            doc="Cost per watt for other equipment, installation, and margin/overhead",
+        )
+
+        blk.cost_per_watt_indirect = pyo.Var(
+            initialize=0.13,
+            units=blk.config.flowsheet_costing_block.base_currency / pyo.units.watt,
+            bounds=(0, None),
+            doc="Cost per watt for permitting, environmental studies, engineering, land prep, and grid interconnection",
+        )
+
+        blk.land_cost_per_acre = pyo.Var(
+            initialize=4000,
+            units=blk.config.flowsheet_costing_block.base_currency / pyo.units.acre,
+            bounds=(0, None),
+            doc="Land cost per acre required",
+        )
+
+        blk.land_area = pyo.Var(
+            initialize=0,
+            units=pyo.units.acre,
+            bounds=(0, None),
+            doc="Land area required for PV system",
+        )
+
+        blk.contingency_frac_direct_cost = pyo.Var(
+            initialize=0.03,
+            units=pyo.units.dimensionless,
+            bounds=(0, 1),
+            doc="Fraction of direct costs for contingency",
+        )
+
+        blk.tax_frac_direct_cost = pyo.Var(
+            initialize=0.05,
+            units=pyo.units.dimensionless,
+            bounds=(0, 1),
+            doc="Fraction of direct costs for sales tax",
+        )
+
+        blk.direct_cost_constraint = pyo.Constraint(
+            expr=blk.direct_cost
+            == blk.system_capacity
+            * (
+                blk.cost_per_watt_module
+                + blk.cost_per_watt_inverter
+                + blk.cost_per_watt_other
+            )
+            + (
+                blk.system_capacity
+                * (
+                    blk.cost_per_watt_module
+                    + blk.cost_per_watt_inverter
+                    + blk.cost_per_watt_other
+                )
+            )
+            * blk.contingency_frac_direct_cost
+        )
+
+        blk.indirect_cost_constraint = pyo.Constraint(
+            expr=blk.indirect_cost
+            == (blk.system_capacity * blk.cost_per_watt_indirect)
+            + (blk.land_area * blk.land_cost_per_acre)
+        )
+
+        blk.sales_tax_constraint = pyo.Constraint(
+            expr=blk.sales_tax == blk.direct_cost * blk.tax_frac_direct_cost
+        )
 
         blk.capital_cost_constraint = pyo.Constraint(
-            expr=blk.capital_cost == blk.installed_cost
+            expr=blk.capital_cost == blk.direct_cost + blk.indirect_cost + blk.sales_tax
         )
 
         blk.fixed_operating_cost = pyo.Var(
@@ -60,15 +164,6 @@ class SETOZeroOrderCostingData(ZeroOrderCostingData):
             doc="Fixed operating cost of solar system",
         )
 
-        blk.system_capacity = pyo.Var(
-            initialize=0,
-            units=pyo.units.kW,
-            bounds=(0, None),
-            doc="Capacity of solar system",
-        )
-
-        blk.system_capacity.fix()
-
         blk.fixed_operating_by_capacity = pyo.Var(
             initialize=0,
             units=blk.config.flowsheet_costing_block.base_currency
@@ -77,11 +172,9 @@ class SETOZeroOrderCostingData(ZeroOrderCostingData):
             doc="Fixed operating cost of solar system per kW generated",
         )
 
-        blk.fixed_operating_by_capacity.fix()
-
         blk.fixed_operating_cost_constraint = pyo.Constraint(
             expr=blk.fixed_operating_cost
-            == blk.fixed_operating_by_capacity * blk.system_capacity
+            == blk.fixed_operating_by_capacity * pyo.units.convert(blk.system_capacity, to_units=pyo.units.kW)
         )
 
         blk.variable_operating_cost = pyo.Var(
@@ -99,8 +192,6 @@ class SETOZeroOrderCostingData(ZeroOrderCostingData):
             doc="Annual electricity generation of solar system",
         )
 
-        blk.annual_generation.fix()
-
         blk.variable_operating_by_generation = pyo.Var(
             initialize=0,
             units=blk.config.flowsheet_costing_block.base_currency
@@ -108,8 +199,6 @@ class SETOZeroOrderCostingData(ZeroOrderCostingData):
             bounds=(0, None),
             doc="Annual operating cost of solar system per MWh generated",
         )
-
-        blk.variable_operating_by_generation.fix()
 
         blk.variable_operating_cost_constraint = pyo.Constraint(
             expr=blk.variable_operating_cost
