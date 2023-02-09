6f84f97700f8f4477e7a483fca21c23d162f5b7f
kurbansitterley <kurban.sitterley@nrel.gov>
2023-02-09 16:44:53 -0700

    initial ec commit

---
+++ src/watertap_contrib/seto/unit_models/electrocoagulation.py
@@ -0,0 +1,315 @@
+###############################################################################
+# WaterTAP Copyright (c) 2021, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National
+# Laboratory, National Renewable Energy Laboratory, and National Energy
+# Technology Laboratory (subject to receipt of any required approvals from
+# the U.S. Dept. of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#
+###############################################################################
+
+from copy import deepcopy
+
+# Import Pyomo libraries
+from pyomo.environ import (
+    Set,
+    Var,
+    check_optimal_termination,
+    Param,
+    Suffix,
+    log,
+    units as pyunits,
+)
+from pyomo.common.config import ConfigBlock, ConfigValue, In
+
+# Import IDAES cores
+from idaes.core import (
+    declare_process_block_class,
+    UnitModelBlockData,
+    useDefault,
+)
+from idaes.core.solvers.get_solver import get_solver
+from idaes.core.util.tables import create_stream_table_dataframe
+from idaes.core.util.constants import Constants
+from idaes.core.util.config import is_physical_parameter_block
+from idaes.core.util.misc import StrEnum
+
+from idaes.core.util.exceptions import ConfigurationError, InitializationError
+import idaes.core.util.scaling as iscale
+import idaes.logger as idaeslog
+
+from watertap.core import InitializationMixin
+
+__author__ = "Kurban Sitterley"
+
+_log = idaeslog.getLogger(__name__)
+
+
+class ElectrodeMaterial(StrEnum):
+    aluminum = "aluminum"
+    iron = "iron"
+
+
+@declare_process_block_class("Electrocoagulation")
+class ElectrocoagulationData(InitializationMixin, UnitModelBlockData):
+    """
+    Zero order electrocoagulation model
+    """
+
+    CONFIG = ConfigBlock()
+
+    CONFIG.declare(
+        "dynamic",
+        ConfigValue(
+            domain=In([False]),
+            default=False,
+            description="Dynamic model flag - must be False",
+            doc="""Indicates whether this model will be dynamic or not,
+    **default** = False.""",
+        ),
+    )
+
+    CONFIG.declare(
+        "has_holdup",
+        ConfigValue(
+            default=False,
+            domain=In([False]),
+            description="Holdup construction flag - must be False",
+            doc="""Indicates whether holdup terms should be constructed or not.
+    **default** - False.""",
+        ),
+    )
+
+    CONFIG.declare(
+        "property_package",
+        ConfigValue(
+            default=useDefault,
+            domain=is_physical_parameter_block,
+            description="Property package to use for control volume",
+            doc="""Property parameter object used to define property calculations,
+    **default** - useDefault.
+    **Valid values:** {
+    **useDefault** - use default package from parent model or flowsheet,
+    **PhysicalParameterObject** - a PhysicalParameterBlock object.}""",
+        ),
+    )
+
+    CONFIG.declare(
+        "property_package_args",
+        ConfigBlock(
+            implicit=True,
+            description="Arguments to use for constructing property packages",
+            doc="""A ConfigBlock with arguments to be passed to a property block(s)
+    and used when constructing these,
+    **default** - None.
+    **Valid values:** {
+    see property package for documentation.}""",
+        ),
+    )
+
+    CONFIG.declare(
+        "electrode_material",
+        ConfigValue(default="aluminum", domain=In(ElectrodeMaterial), description="Electrode material"),
+    )
+
+    def build(self):
+        super().build()
+
+        
+
+        if self.config.electrode_material == ElectrodeMaterial.aluminum:
+            self.mw_electrode_material = Param(initialize=0.027, units=pyunits.kg/pyunits.mol, doc="Molecular weight of electrode material")
+            self.valence_electrode_material = Param(initialize=3, units=pyunits.dimensionless, doc="Number of valence electrons of electrode material")
+            self.density_electrode_material = Param(initialize=1
+                                                    , units=pyunits.kg/pyunits.m**3, doc="Density of electrode material")
+
+        if self.config.electrode_material == ElectrodeMaterial.iron:
+            self.mw_electrode_material = Param(initialize=1)
+
+        self.scaling_factor = Suffix(direction=Suffix.EXPORT)
+
+        self.conductivity = Var(initialize=20, units=pyunits.S/pyunits.m, doc="Solution conductivity")
+
+        self.electrode_width = Var(
+            initialize=1,
+            units=pyunits.m,
+            doc="Electrode width",
+        )
+
+        self.electrode_height = Var(
+            initialize=1,
+            units=pyunits.m,
+            doc="Electrode height",
+        )
+
+        self.electrode_thick = Var(
+            initialize=1,
+            units=pyunits.m,
+            doc="Electrode thickness",
+        )
+
+        self.applied_current = Var(
+            initialize=1,
+            units=pyunits.ampere,
+            doc="Applied current",
+        )
+
+        self.current_efficiency = Var(
+            initialize=1,
+            units=pyunits.ampere,
+            doc="Current efficiency",
+        )
+
+        self.cell_voltage = Var(
+            initialize=1,
+            units=pyunits.volt,
+            doc="Cell voltage",
+        )
+
+
+
+        tmp_dict = dict(**self.config.property_package_args)
+        tmp_dict["has_phase_equilibrium"] = False
+        tmp_dict["parameters"] = self.config.property_package
+        tmp_dict["defined_state"] = True  # inlet block is an inlet
+        self.properties_in = self.config.property_package.state_block_class(
+            self.flowsheet().config.time, doc="Material properties of inlet", **tmp_dict
+        )
+
+        # Add outlet and waste block
+        tmp_dict["defined_state"] = False  
+        self.properties_out = self.config.property_package.state_block_class(
+            self.flowsheet().config.time,
+            doc="Material properties of outlet",
+            **tmp_dict,
+        )
+
+        self.properties_waste = self.config.property_package.state_block_class(
+            self.flowsheet().config.time, doc="Material properties of waste", **tmp_dict
+        )
+
+        self.add_port(name="inlet", block=self.properties_in)
+        self.add_port(name="outlet", block=self.properties_out)
+        self.add_port(name="waste", block=self.properties_regen)
+
+        # =========== EQUILIBRIUM ===========
+
+    def initialize_build(
+        blk,
+        state_args=None,
+        outlvl=idaeslog.NOTSET,
+        solver=None,
+        optarg=None,
+    ):
+        """
+        General wrapper for initialization routines
+
+        Keyword Arguments:
+            state_args : a dict of arguments to be passed to the property
+                         package(s) to provide an initial state for
+                         initialization (see documentation of the specific
+                         property package) (default = {}).
+            outlvl : sets output level of initialization routine
+            optarg : solver options dictionary object (default=None)
+            solver : str indicating which solver to use during
+                     initialization (default = None)
+
+        Returns: None
+        """
+        init_log = idaeslog.getInitLogger(blk.name, outlvl, tag="unit")
+        solve_log = idaeslog.getSolveLogger(blk.name, outlvl, tag="unit")
+
+        opt = get_solver(solver, optarg)
+
+        # ---------------------------------------------------------------------
+        flags = blk.properties_in.initialize(
+            outlvl=outlvl,
+            optarg=optarg,
+            solver=solver,
+            state_args=state_args,
+            hold_state=True,
+        )
+        init_log.info("Initialization Step 1a Complete.")
+        # ---------------------------------------------------------------------
+        # Initialize other state blocks
+        # Set state_args from inlet state
+        if state_args is None:
+            blk.state_args = state_args = {}
+            state_dict = blk.properties_in[
+                blk.flowsheet().config.time.first()
+            ].define_port_members()
+
+            for k in state_dict.keys():
+                if state_dict[k].is_indexed():
+                    state_args[k] = {}
+                    for m in state_dict[k].keys():
+                        state_args[k][m] = state_dict[k][m].value
+                else:
+                    state_args[k] = state_dict[k].value
+
+        state_args_out = deepcopy(state_args)
+
+        blk.properties_out.initialize(
+            outlvl=outlvl,
+            optarg=optarg,
+            solver=solver,
+            state_args=state_args_out,
+        )
+        init_log.info("Initialization Step 1b Complete.")
+
+        state_args_waste = deepcopy(state_args)
+
+        blk.properties_regen.initialize(
+            outlvl=outlvl,
+            optarg=optarg,
+            solver=solver,
+            state_args=state_args_waste,
+        )
+
+        init_log.info("Initialization Step 1c Complete.")
+
+        # Solve unit
+        with idaeslog.solver_log(solve_log, idaeslog.DEBUG) as slc:
+            res = opt.solve(blk, tee=slc.tee)
+        init_log.info("Initialization Step 2 {}.".format(idaeslog.condition(res)))
+        # ---------------------------------------------------------------------
+        # Release Inlet state
+        blk.properties_in.release_state(flags, outlvl=outlvl)
+        init_log.info("Initialization Complete: {}".format(idaeslog.condition(res)))
+
+        if not check_optimal_termination(res):
+            raise InitializationError(f"Unit model {blk.name} failed to initialize")
+
+    def calculate_scaling_factors(self):
+        super().calculate_scaling_factors()
+
+        iscale.set_scaling_factor(self.Re, 1)
+
+        # transforming constraints
+        for ind, c in self.eq_partition_ratio.items():
+            sf = iscale.get_scaling_factor(
+                self.properties_in[0].conc_equiv_phase_comp[
+                    "Liq", self.config.target_ion
+                ]
+            )
+            iscale.constraint_scaling_transform(c, sf)
+
+    def _get_stream_table_contents(self, time_point=0):
+        return create_stream_table_dataframe(
+            {
+                "Feed Inlet": self.inlet,
+                "Liquid Outlet": self.outlet,
+                "Waste Outlet": self.waste,
+            },
+            time_point=time_point,
+        )
+
+    def _get_performance_contents(self, time_point=0):
+
+        # TODO
+        var_dict = {}
+
+        return {"vars": var_dict}
