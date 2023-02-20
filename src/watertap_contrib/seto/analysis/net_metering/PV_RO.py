461664aa017d3607fd1132c5fb250cdf45a7f1fc
kurbansitterley <kurban.sitterley@nrel.gov>
2023-02-20 13:23:22 -0700

    apply changes to PV_RO and test file

--- src/watertap_contrib/seto/analysis/net_metering/PV_RO.py
+++ src/watertap_contrib/seto/analysis/net_metering/PV_RO.py
@@ -47,7 +47,7 @@ from watertap_contrib.seto.costing import (
     EnergyCosting,
     SETOSystemCosting,
 )
-from watertap_contrib.seto.solar_models.zero_order import PhotovoltaicZO
+from watertap_contrib.seto.solar_models.zero_order import Photovoltaic
 from watertap_contrib.seto.core import SETODatabase, PySAMWaterTAP
 
 
@@ -90,7 +90,7 @@ def build_ro_pv():
     treatment = m.fs.treatment = Block()
     energy = m.fs.energy = Block()
 
-    energy.pv = PhotovoltaicZO(property_package=m.fs.properties, database=m.db)
+    energy.pv = Photovoltaic()
     treatment.feed = Feed(property_package=m.fs.properties)
     treatment.product = Product(property_package=m.fs.properties)
     treatment.disposal = Product(property_package=m.fs.properties)
@@ -127,8 +127,8 @@ def build_ro_pv():
     treatment.feed.properties[0].flow_vol_phase["Liq"]
     treatment.feed.properties[0].mass_frac_phase_comp["Liq", "NaCl"]
 
-    energy.pv.properties[0].flow_vol_phase["Liq"]
-    energy.pv.properties[0].mass_frac_phase_comp["Liq", "NaCl"]
+    # energy.pv.properties[0].flow_vol_phase["Liq"]
+    # energy.pv.properties[0].mass_frac_phase_comp["Liq", "NaCl"]
 
     set_scaling_factor(treatment.erd.control_volume.work, 1e-3)
 
@@ -153,13 +153,13 @@ def set_operating_conditions(m, flow_in=1e-2, conc_in=30, water_recovery=0.5):
         },
         hold_state=True,  # fixes the calculated component mass flow rates
     )
-    m.fs.energy.pv.properties.calculate_state(
-        var_args={
-            ("flow_vol_phase", "Liq"): flow_in,  # feed volumetric flow rate [m3/s]
-            ("mass_frac_phase_comp", ("Liq", "NaCl")): conc_in * 1e-3,
-        },
-        hold_state=False,  # fixes the calculated component mass flow rates
-    )
+    # m.fs.energy.pv.properties.calculate_state(
+    #     var_args={
+    #         ("flow_vol_phase", "Liq"): flow_in,  # feed volumetric flow rate [m3/s]
+    #         ("mass_frac_phase_comp", ("Liq", "NaCl")): conc_in * 1e-3,
+    #     },
+    #     hold_state=False,  # fixes the calculated component mass flow rates
+    # )
 
     operating_pressure = calculate_operating_pressure(
         feed_state_block=m.fs.treatment.feed.properties[0],
@@ -188,15 +188,15 @@ def set_operating_conditions(m, flow_in=1e-2, conc_in=30, water_recovery=0.5):
     m.fs.treatment.p1.control_volume.properties_out[0].pressure.fix(operating_pressure)
     m.fs.treatment.p1.efficiency_pump.fix(0.8)
     m.db.get_unit_operation_parameters("solar_energy")
-    m.fs.energy.pv.load_parameters_from_database(use_default_removal=True)
+    # m.fs.energy.pv.load_parameters_from_database(use_default_removal=True)
 
 
 def initialize_treatment(m, water_recovery=0.5):
-    """_summary_
+    """
 
     Args:
         m (obj): Pyomo model
-        water_recovery (float, optional): waater recovery. Defaults to 0.5.
+        water_recovery (float, optional): water recovery. Defaults to 0.5.
     """
     ro = m.fs.treatment.ro
     p1 = m.fs.treatment.p1
@@ -273,7 +273,7 @@ def initialize_treatment(m, water_recovery=0.5):
 
 
 def initialize_energy(m):
-    m.fs.energy.pv.initialize()
+    # m.fs.energy.pv.initialize()
     m.fs.energy.pv.oversize_factor = 1
 
 
@@ -337,7 +337,7 @@ def add_costing(m):
     energy.costing = EnergyCosting()
 
     m.db.get_unit_operation_parameters("photovoltaic")
-    energy.pv.load_parameters_from_database(use_default_removal=True)
+    # energy.pv.load_parameters_from_database(use_default_removal=True)
 
     energy.pv.costing = UnitModelCostingBlock(flowsheet_costing_block=energy.costing)
     treatment.ro.costing = UnitModelCostingBlock(
--- src/watertap_contrib/seto/analysis/tests/test_RO_PV.py
+++ src/watertap_contrib/seto/analysis/tests/test_RO_PV.py
@@ -67,7 +67,7 @@ class TestPVRO:
         assert stats.number_unused_variables(m) <= 3
         assert stats.number_fixed_variables(m.fs.treatment.p1.control_volume.properties_out[0]) == 1
         assert stats.number_total_blocks(m.fs.treatment) == 29
-        assert stats.number_total_blocks(m.fs.energy) == 3
+        assert stats.number_total_blocks(m.fs.energy) == 2
 
     @pytest.mark.unit
     def test_initialization(self, system_frame):
@@ -79,17 +79,17 @@ class TestPVRO:
         PV_RO.initialize_treatment(m)
         PV_RO.initialize_energy(m)
 
-        assert stats.degrees_of_freedom(m) == 2
-        assert stats.number_variables(m) == 212
-        assert stats.number_total_constraints(m) == 174
-        assert stats.number_unused_variables(m) == 3
+        assert stats.degrees_of_freedom(m) == 0
+        assert stats.number_variables(m) == 205
+        assert stats.number_total_constraints(m) == 170
+        assert stats.number_unused_variables(m) == 2
 
         for component in ['feed','product','disposal','p1', 'ro', 'erd']:
             assert hasattr(m.fs.treatment, component)
             unit = getattr(m.fs.treatment, component)
             assert_units_consistent(unit)
 
-        initialization_tester(m, unit=m.fs.treatment.ro, dof=2, outlvl=idaeslog.DEBUG)
+        initialization_tester(m, unit=m.fs.treatment.ro, dof=0, outlvl=idaeslog.DEBUG)
 
     @pytest.mark.unit
     def test_costing(self, system_frame):
