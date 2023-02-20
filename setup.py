e7c69fe8416169f37210094960bbefc00ec0e161
kurbansitterley <kurban.sitterley@nrel.gov>
2023-02-20 15:09:38 -0700

    reformat setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ setup(
     author="WaterTAP-SETO contributors",
     python_requires=">=3.8",
     install_requires=[
-        # "watertap == 0.7.0",
-        "watertap @ https://github.com/watertap-org/watertap/archive/main.zip", 
+        "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
         "pytest >= 7",
         "nrel-pysam == 3.0.2",
     ],
