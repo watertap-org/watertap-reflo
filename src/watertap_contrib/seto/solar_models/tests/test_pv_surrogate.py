2be0737bbee3ba65b265d99943d0d92e727da323
Zach Binger <zbinger@nrel.gov>
2023-02-23 16:47:37 -0700

    added surrogate eval to test file

--- src/watertap_contrib/seto/solar_models/tests/test_pv_surrogate.py
+++ src/watertap_contrib/seto/solar_models/tests/test_pv_surrogate.py
@@ -36,19 +36,10 @@ class TestPVSurrogate:
     def test_create_surrogate(self, pv_frame):
         m = pv_frame
         m.fs.energy.pv.create_surrogate()
-    #     # create input and output variable object lists for flowsheet
-    #     inputs = [m.fs.design_size]
-    #     outputs = [m.fs.annual_energy]
 
-    #     # load surrogate file and build model
-    #     m.fs.surrogate = SurrogateBlock(concrete=True)
-    #     surrogate = PysmoSurrogate.load_from_file(SURROGATE_FILENAME)
-    #     m.fs.surrogate.build_model(surrogate, input_vars=inputs, output_vars=outputs)
-
-    # def test_eval_surrogate(self, pv_frame):
-    #     # fix input values and solve flowsheet
-    #     m = pv_frame
-    #     results = solver.solve(m)
-
-    #     assert pytest.approx(5000, rel=1e-2) == value(value(m.fs.design_size))
-    #     assert pytest.approx(1.13e7, rel=1e-2) == value(value(m.fs.annual_energy))
+    def test_eval_surrogate(self, pv_frame):
+        # fix input values and solve flowsheet
+        m = pv_frame
+        results = solver.solve(m)
+        assert pytest.approx(5000, rel=1e-2) == value(value(m.fs.energy.pv.design_size))
+        assert pytest.approx(1.13e7, rel=1e-2) == value(value(m.fs.energy.pv.annual_energy))
