752dfaab244f2301452e7c9544d41b9f1b2165e2
kurbansitterley <kurban.sitterley@nrel.gov>
2023-04-10 15:44:44 -0600

    update trough_LTMED for solve

--- src/watertap_contrib/seto/analysis/net_metering/trough_LTMED.py
+++ src/watertap_contrib/seto/analysis/net_metering/trough_LTMED.py
@@ -30,49 +30,15 @@ from watertap_contrib.seto.unit_models.surrogate import LTMEDSurrogate
 solver = get_solver()
 
 
-def main():
-    build_solve_trough()
-    build_solve_lt_med()
-    build_solve_trough_lt_med()
+def build_trough_lt_med(
+    salinity=35,
+    recovery=0.5,
+    feed_temp=25,
+    steam_temp=80,
+    capacity=80000,
+):
+    feed_dens = 1000 * pyunits.kg / pyunits.m**3  # g/L = kg/m3
 
-
-def check_scaling(m, jacob=False):
-    if jacob:
-        jac, nlp = get_jacobian(m, scaled=False)
-        print("Extreme Jacobian entries:")
-        for i in extreme_jacobian_entries(jac=jac, nlp=nlp, large=100):
-            print(f"derivative of: {i[1]}\nwith respect to: {i[2]}\n\t{i[0]:.2e}\n")
-    print("Badly scaled variables:")
-    for v, sv in badly_scaled_var_generator(m, large=1e2, small=1e-2, zero=1e-12):
-        print(f"var: {v}\n\tscaled value: {sv}\n\tsf: {get_scaling_factor(v)}")
-
-
-def build_solve_trough():
-
-    m = ConcreteModel()
-    m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.energy = Block()
-    m.fs.energy.trough = TroughSurrogate()
-    m.fs.energy.costing = EnergyCosting()
-    m.fs.energy.trough.costing = UnitModelCostingBlock(
-        flowsheet_costing_block=m.fs.energy.costing
-    )
-    m.fs.energy.costing.cost_process()
-
-    trough = m.fs.energy.trough
-    trough.heat_load.fix(216)
-    trough.hours_storage.fix(6)
-
-    print(f"DOF = {degrees_of_freedom(m)}")
-    results = solver.solve(m)
-    assert_optimal_termination(results)
-
-    print(f"Trough capital: ${m.fs.energy.costing.total_capital_cost()}")
-    print(f"Annual heat generated: {m.fs.energy.trough.heat_annual()} kWh")
-    print(f"Annual electricity consumed: {m.fs.energy.trough.electricity_annual()} kWh")
-
-
-def build_solve_lt_med():
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
     m.fs.water_prop = SeawaterParameterBlock()
@@ -88,110 +54,50 @@ def build_solve_lt_med():
     steam = lt_med.steam_props[0]
 
     # System specification
-    feed_salinity = 35 * pyunits.kg / pyunits.m**3  # g/L = kg/m3
-    feed_dens = 1000 * pyunits.kg / pyunits.m**3  # g/L = kg/m3
-    feed_temperature = 25  # degC
-    steam_temperature = 80  # degC
-    sys_capacity = 80000 * pyunits.m**3 / pyunits.day  # m3/day
-    recovery_ratio = 0.5 * pyunits.dimensionless  # dimensionless
+    feed_salinity = salinity * pyunits.kg / pyunits.m**3  # g/L = kg/m3
+
+    sys_capacity = capacity * pyunits.m**3 / pyunits.day  # m3/day
+    recovery_ratio = recovery * pyunits.dimensionless  # dimensionless
     feed_flow = pyunits.convert(
         (sys_capacity / recovery_ratio), to_units=pyunits.m**3 / pyunits.s
     )
 
     feed.flow_mass_phase_comp["Liq", "TDS"].fix(feed_salinity * feed_flow)
     feed.flow_mass_phase_comp["Liq", "H2O"].fix(feed_dens * feed_flow)
-    feed.temperature.fix(feed_temperature + 273.15)
-    steam.temperature.fix(steam_temperature + 273.15)
+
+    feed.temperature.fix(feed_temp + 273.15)
+    steam.temperature.fix(steam_temp + 273.15)
     # flow rate of liquid steam is zero
     steam.flow_mass_phase_comp["Liq", "H2O"].fix(0)
     dist.flow_mass_phase_comp["Liq", "TDS"].fix(0)  # salinity in distillate is zero
+    # dist.flow_mass_phase_comp["Liq", "H2O"].fix(feed_dens * dist_flow)
 
     lt_med.recovery_vol_phase[0, "Liq"].fix(recovery_ratio)
+    # lt_med.gain_output_ratio.fix(9.913)
+
     m.fs.treatment.costing = TreatmentCosting()
     m.fs.treatment.lt_med.costing = UnitModelCostingBlock(
         flowsheet_costing_block=m.fs.treatment.costing
     )
     m.fs.treatment.costing.cost_process()
-    m.fs.water_prop.set_default_scaling(
-        "flow_mass_phase_comp", 1e-3, index=("Liq", "H2O")
-    )
-    m.fs.water_prop.set_default_scaling(
-        "flow_mass_phase_comp", 1e-3, index=("Liq", "TDS")
-    )
-    m.fs.steam_prop.set_default_scaling(
-        "flow_mass_phase_comp", 1e-3, index=("Liq", "H2O")
-    )
-    m.fs.steam_prop.set_default_scaling(
-        "flow_mass_phase_comp", 1e-3, index=("Vap", "H2O")
-    )
-    set_scaling_factor(lt_med.thermal_power_requirement, 1e-6)
-    calculate_scaling_factors(m)
-    # check_scaling(m)
-    lt_med.initialize()
-    check_scaling(m)
-    results = solver.solve(m)
-    print(f"DOF = {degrees_of_freedom(m)}")
-    assert_optimal_termination(results)
-    print(f"LT-MED capital: ${m.fs.treatment.costing.total_capital_cost()}")
-    print(f"LT-MED thermal power requirement: {lt_med.thermal_power_requirement()} kW")
-
-
-def build_solve_trough_lt_med():
-    m = ConcreteModel()
-    m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.water_prop = SeawaterParameterBlock()
-    m.fs.steam_prop = WaterParameterBlock()
-    m.fs.treatment = Block()
-    m.fs.treatment.lt_med = LTMEDSurrogate(
-        property_package_water=m.fs.water_prop,
-        property_package_steam=m.fs.steam_prop,
-    )
-    lt_med = m.fs.treatment.lt_med
-    feed = lt_med.feed_props[0]
-    dist = lt_med.distillate_props[0]
-    steam = lt_med.steam_props[0]
-
-    # System specification
-    feed_salinity = 35 * pyunits.kg / pyunits.m**3  # g/L = kg/m3
-    feed_dens = 1000 * pyunits.kg / pyunits.m**3  # g/L = kg/m3
-    feed_temperature = 25  # degC
-    steam_temperature = 80  # degC
-    sys_capacity = 80000 * pyunits.m**3 / pyunits.day  # m3/day
-    recovery_ratio = 0.5 * pyunits.dimensionless  # dimensionless
-    feed_flow = pyunits.convert(
-        (sys_capacity / recovery_ratio), to_units=pyunits.m**3 / pyunits.s
-    )
-
-    feed.flow_mass_phase_comp["Liq", "TDS"].fix(feed_salinity * feed_flow)
-    feed.flow_mass_phase_comp["Liq", "H2O"].fix(feed_dens * feed_flow)
-    feed.temperature.fix(feed_temperature + 273.15)
-    steam.temperature.fix(steam_temperature + 273.15)
-    # flow rate of liquid steam is zero
-    steam.flow_mass_phase_comp["Liq", "H2O"].fix(0)
-    dist.flow_mass_phase_comp["Liq", "TDS"].fix(0)  # salinity in distillate is zero
-
-    lt_med.recovery_vol_phase[0, "Liq"].fix(recovery_ratio)
-    m.fs.treatment.costing = TreatmentCosting()
-    m.fs.treatment.lt_med.costing = UnitModelCostingBlock(
-        flowsheet_costing_block=m.fs.treatment.costing
-    )
-    m.fs.treatment.costing.cost_process()
+    m.fs.treatment.costing.add_LCOW(dist.flow_vol_phase["Liq"])
 
     m.fs.energy = Block()
     m.fs.energy.trough = TroughSurrogate()
     trough = m.fs.energy.trough
+    trough.hours_storage.fix(24)
     m.fs.energy.costing = EnergyCosting()
     m.fs.energy.trough.costing = UnitModelCostingBlock(
         flowsheet_costing_block=m.fs.energy.costing
     )
-
     m.fs.energy.costing.cost_process()
     m.fs.sys_costing = SETOSystemCosting()
+    m.fs.sys_costing.add_LCOW(dist.flow_vol_phase["Liq"])
 
     m.fs.energy.lt_med_heat_demand_constr = Constraint(
-        expr=trough.heat_load == lt_med.thermal_power_requirement
+        expr=trough.heat_load
+        == pyunits.convert(lt_med.thermal_power_requirement, to_units=pyunits.MW)
     )
-    trough.hours_storage.fix(6)
 
     m.fs.water_prop.set_default_scaling(
         "flow_mass_phase_comp", 1e-3, index=("Liq", "H2O")
@@ -205,18 +111,67 @@ def build_solve_trough_lt_med():
     m.fs.steam_prop.set_default_scaling(
         "flow_mass_phase_comp", 1e-3, index=("Vap", "H2O")
     )
-    set_scaling_factor(lt_med.thermal_power_requirement, 1e-6)
-
+    # set_scaling_factor(lt_med.thermal_power_requirement, 1e-6)
+    # constraint_scaling_transform(lt_med.eq_specific_thermal_energy_consumption, 1e6)
     calculate_scaling_factors(m)
+    # calculate_variable_from_constraint(lt_med.gain_output_ratio, lt_med.eq_gain_output_ratio)
+    # calculate_variable_from_constraint(lt_med.specific_area, lt_med.eq_specific_area)
     lt_med.initialize()
-
+    # trough.initialize()
     # check_scaling(m)
-    results = solver.solve(m)
-    print(f"DOF = {degrees_of_freedom(m)}")
-    print(f"SOLVE = {results.solver.termination_condition.swapcase()}")
 
-    print_infeasible_constraints(m)
+    # print_infeasible_constraints(m)
+
+    return m
 
 
-if __name__ == "__main__":
-    main()
+m = build_trough_lt_med()
+trough = m.fs.energy.trough
+lt_med = m.fs.treatment.lt_med
+# m.fs.energy.costing.heat_cost.set_value(0)
+# m.fs.treatment.costing.heat_cost.set_value(0)
+# m.fs.energy.costing.electricity_cost.set_value(0.001)
+# m.fs.treatment.costing.electricity_cost.set_value(0.001)
+# m.fs.sys_costing.heat_cost.set_value(0)
+m.fs.sys_costing.obj = Objective(expr=m.fs.sys_costing.LCOW)
+
+# for v in m.fs.component_objects(Var):
+#     if "costing" in v.name:
+#         set_scaling_factor(v, 1e-6)
+# check_scaling(m, jacob=True)
+# m.fs.energy.trough.heat.setub(0)
+constraint_scaling_transform(
+    m.fs.energy.trough.surrogate_blk.pysmo_constraint["heat_annual"], 1e-3
+)
+constraint_scaling_transform(
+    m.fs.energy.trough.surrogate_blk.pysmo_constraint["electricity_annual"], 1e-3
+)
+
+m.fs.treatment.lt_med.distillate_props[0].flow_vol_phase["Liq"].fix()
+print(f"DOF = {degrees_of_freedom(m)}")
+
+solver = get_solver()
+solver.options["max_iter"] = 10000
+solver.options["halt_on_ampl_error"] = "yes"
+results = solver.solve(
+    m,
+    symbolic_solver_labels=True,
+    tee=False,
+)
+print(f"SOLVE - {results.solver.termination_condition}")
+print_infeasible_constraints(m)
+
+print(f"{results.solver.termination_condition}")
+# m.fs.energy.trough.heat_load.fix()
+
+
+results = solver.solve(m)
+print(m.fs.treatment.lt_med.thermal_power_requirement())
+print(trough.heat_load())
+print(f"DOF = {degrees_of_freedom(m)}")
+print(f"SOLVE - {results.solver.termination_condition}")
+print_infeasible_constraints(m)
+# print(m.fs.treatment.lt_med.costing.capacity())
+# m.fs.treatment.lt_med.feed_props[0].display()
+# m.fs.treatment.lt_med.distillate_props[0].display()
+print(m.fs.sys_costing.LCOW())
