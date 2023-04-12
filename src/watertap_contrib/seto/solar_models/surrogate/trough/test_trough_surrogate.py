e45ec64fa7ae52efe22bc905b4b0a8e542c8bca9
kurbansitterley <kurban.sitterley@nrel.gov>
2023-04-12 14:23:11 -0600

    finalize surrogate test file

--- src/watertap_contrib/seto/solar_models/surrogate/trough/test_trough_surrogate.py
+++ src/watertap_contrib/seto/solar_models/surrogate/trough/test_trough_surrogate.py
@@ -1,18 +1,18 @@
 import pytest
 import os
-import sys
-from io import StringIO
-from os.path import join, dirname, getsize
+
 import pandas as pd
-from pyomo.environ import ConcreteModel, assert_optimal_termination, Var, value
+from pyomo.environ import (
+    ConcreteModel,
+    SolverFactory,
+    Var,
+    value,
+    assert_optimal_termination,
+)
 from pyomo.network import Port
 
-from watertap_contrib.seto.solar_models.surrogate.trough.data.training_trough_surrogate import (
-    create_rbf_surrogate,
-)
 from watertap_contrib.seto.solar_models.surrogate.trough import TroughSurrogate
-from watertap_contrib.seto.core import SolarEnergyBase
-from watertap_contrib.seto.costing import SETOWaterTAPCosting, EnergyCosting
+from watertap_contrib.seto.costing import EnergyCosting
 
 from idaes.core.solvers import get_solver
 from idaes.core.surrogate.pysmo_surrogate import PysmoSurrogate
@@ -24,14 +24,14 @@ from idaes.core.util.model_statistics import (
     number_total_constraints,
     number_unused_variables,
 )
+from idaes.core.util.scaling import (
+    calculate_scaling_factors,
+    unscaled_variables_generator,
+)
 
-dataset_filename = join(dirname(__file__), "data/trough_data.pkl")
-surrogate_filename = join(dirname(__file__), "trough_surrogate.json")
-N_SAMPLES = 100  # number of points to use from overall dataset
-TRAINING_FRACTION = 0.8
-INPUT_LABELS = ["heat_load", "hours_storage"]
-OUTPUT_LABELS = ["heat_annual", "electricity_annual"]
-EXPECTED_HEAT_ENERGIES = [
+dataset_filename = os.path.join(os.path.dirname(__file__), "data/trough_data.pkl")
+surrogate_filename = os.path.join(os.path.dirname(__file__), "trough_surrogate.json")
+expected_heat_annual = [
     7.997e8,
     5.259e8,
     5.753e8,
@@ -43,7 +43,7 @@ EXPECTED_HEAT_ENERGIES = [
     8.285e8,
     8.563e8,
 ]
-EXPECTED_ELECTRICITY_USE = [
+expected_electricity_annual = [
     1.534e7,
     4.599e6,
     4.014e6,
@@ -57,28 +57,25 @@ EXPECTED_ELECTRICITY_USE = [
 ]
 
 
-class TestTrough:
-    @pytest.fixture(scope="class")
-    def data(self):
-        # Read data manually for repeatability instead of using the training/validation function with random splits
-        df = pd.read_pickle(dataset_filename)
-        return {"training": df[:80], "validation": df[80:90]}
+def get_data():
+    df = pd.read_pickle(dataset_filename)
+    return {"training": df[:80], "validation": df[80:90]}
 
+
+class TestTrough:
     @pytest.fixture(scope="class")
     def trough_frame(self):
 
         m = ConcreteModel()
         m.fs = FlowsheetBlock(dynamic=False)
         m.fs.trough = TroughSurrogate()
-        m.fs.trough.heat_load.fix(500)
-        m.fs.trough.hours_storage.fix(12)
 
         return m
-    
+
     @pytest.mark.unit
     def test_build(self, trough_frame):
         m = trough_frame
-        
+
         assert len(m.fs.trough.config) == 3
         assert not m.fs.trough.config.dynamic
         assert not m.fs.trough.config.has_holdup
@@ -118,98 +115,113 @@ class TestTrough:
         assert m.fs.trough.heat_load.bounds == tuple([100, 1000])
         assert m.fs.trough.hours_storage.bounds == tuple([0, 26])
 
+    @pytest.mark.component
+    def test_create_rbf_surrogate(self, trough_frame):
+        expected_heat_annual_test = [
+            8.034e8,
+            3.505e8,
+            3.668e8,
+            3.814e8,
+            3.946e8,
+            4.065e8,
+            4.175e8,
+            4.275e8,
+            4.366e8,
+            4.445e8,
+        ]
+        expected_electricity_annual_test = [
+            7.176e6,
+            1.301e6,
+            1.302e6,
+            1.303e6,
+            1.304e6,
+            1.305e6,
+            1.306e6,
+            1.307e6,
+            1.308e6,
+            1.308e6,
+        ]
+        m = trough_frame
+        data = get_data()
+        test_surrogate_filename = os.path.join(
+            os.path.dirname(__file__), "test_surrogate.json"
+        )
+        m.fs.trough._create_rbf_surrogate(
+            data_training=data["training"], output_filename=test_surrogate_filename
+        )
+        assert os.path.getsize(test_surrogate_filename) > 1e4
+        os.remove(test_surrogate_filename)
+        assert isinstance(m.fs.trough.rbf_surr, PysmoSurrogate)
+        test_output = m.fs.trough.rbf_surr.evaluate_surrogate(data["validation"])
+        assert list(test_output["heat_annual"]) == pytest.approx(
+            expected_heat_annual_test, 1e-3
+        )
+        assert list(test_output["electricity_annual"]) == pytest.approx(
+            expected_electricity_annual_test, 1e-3
+        )
+
+    @pytest.mark.component
+    def test_validation(self, trough_frame):
+
+        m = trough_frame
+        data = get_data()
+        heat_annual_list = []
+        electricity_annual_list = []
+
+        for row in data["validation"].itertuples():
+            m.fs.trough.heat_load.fix(row.heat_load)
+            m.fs.trough.hours_storage.fix(row.hours_storage)
+            solver = SolverFactory("ipopt")
+            results = solver.solve(m)
+            assert_optimal_termination(results)
+            heat_annual_list.append(value(m.fs.trough.heat_annual))
+            electricity_annual_list.append(value(m.fs.trough.electricity_annual))
+
+        # ensure surrogate model gives same results when inside a flowsheet
+        assert heat_annual_list == pytest.approx(expected_heat_annual, 1e-3)
+        assert electricity_annual_list == pytest.approx(
+            expected_electricity_annual, 1e-3
+        )
 
     @pytest.mark.unit
     def test_dof(self, trough_frame):
+
         m = trough_frame
+        m.fs.trough.heat_load.fix(500)
+        m.fs.trough.hours_storage.fix(12)
         assert degrees_of_freedom(m) == 0
 
+    @pytest.mark.unit
+    def test_calculate_scaling(self, trough_frame):
 
+        m = trough_frame
+        calculate_scaling_factors(m)
+        assert len(list(unscaled_variables_generator(m))) == 0
 
+    @pytest.mark.component
+    def test_costing(self, trough_frame):
+        m = trough_frame
+        m.fs.costing = EnergyCosting()
+        m.fs.trough.costing = UnitModelCostingBlock(
+            flowsheet_costing_block=m.fs.costing
+        )
+        m.fs.costing.factor_maintenance_labor_chemical.fix(0)
+        m.fs.costing.factor_total_investment.fix(1)
+        m.fs.costing.cost_process()
 
+        solver = SolverFactory("ipopt")
+        results = solver.solve(m)
+        assert_optimal_termination(results)
 
-
-    # @pytest.mark.unit
-    # def test_training(self, data):
-    #     """Test the creation and use of a new surrogate model"""
-    #     EXPECTED_HEAT_ENERGIES_TEST = [
-    #         8.034e8,
-    #         3.505e8,
-    #         3.668e8,
-    #         3.814e8,
-    #         3.946e8,
-    #         4.065e8,
-    #         4.175e8,
-    #         4.275e8,
-    #         4.366e8,
-    #         4.445e8,
-    #     ]
-    #     EXPECTED_ELECTRICITY_USE_TEST = [
-    #         7.176e6,
-    #         1.301e6,
-    #         1.302e6,
-    #         1.303e6,
-    #         1.304e6,
-    #         1.305e6,
-    #         1.306e6,
-    #         1.307e6,
-    #         1.308e6,
-    #         1.308e6,
-    #     ]
-
-    #     # Create a new surrogate model and verify it saved to file
-    #     test_surrogate_filename = join(dirname(__file__), "test_surrogate.json")
-    #     test_surrogate = create_rbf_surrogate(
-    #         data["training"], INPUT_LABELS, OUTPUT_LABELS, test_surrogate_filename
-    #     )
-    #     assert isinstance(test_surrogate, PysmoSurrogate)
-    #     assert getsize(test_surrogate_filename) > 1e4
-    #     os.remove(test_surrogate_filename)
-
-    #     # Verify surrogate model output is expected
-    #     output = test_surrogate.evaluate_surrogate(data["validation"])
-    #     assert list(output["heat_annual"]) == pytest.approx(
-    #         EXPECTED_HEAT_ENERGIES_TEST, 1e-3
-    #     )
-    #     assert list(output["electricity_annual"]) == pytest.approx(
-    #         EXPECTED_ELECTRICITY_USE_TEST, 1e-3
-    #     )
-
-    # @pytest.mark.component
-    # def test_loading(self, data):
-    #     """Test the loading and use of the 'official' saved surrogate model"""
-    #     surrogate = PysmoSurrogate.load_from_file(SURROGATE_FILENAME)
-    #     assert isinstance(surrogate, PysmoSurrogate)
-    #     output = surrogate.evaluate_surrogate(data["validation"])
-    #     assert list(output["heat_annual"]) == pytest.approx(
-    #         EXPECTED_HEAT_ENERGIES, 1e-3
-    #     )
-    #     assert list(output["electricity_annual"]) == pytest.approx(
-    #         EXPECTED_ELECTRICITY_USE, 1e-3
-    #     )
-
-    # @pytest.mark.component
-    # def test_costing(self, trough_frame):
-    #     m = trough_frame
-    #     m.fs.costing = EnergyCosting()
-
-    # @pytest.mark.component
-    # def test_flowsheet_use(self, data, trough_frame):
-    #     """Test creating a flowsheet using the surrogate model"""
-    #     m = trough_frame
-    #     heat_annual = []
-    #     electricity_annual = []
-
-    #     # evaluate flowsheet with surrogate model using validation data as input
-    #     for row in data["validation"].itertuples():
-    #         m.fs.heat_load.fix(row.heat_load)
-    #         m.fs.hours_storage.fix(row.hours_storage)
-    #         solver = get_solver()
-    #         results = solver.solve(m)
-    #         assert_optimal_termination(results)
-    #         heat_annual.append(value(m.fs.heat_annual))
-    #         electricity_annual.append(value(m.fs.electricity_annual))
-
-    #     # ensure surrogate model gives same results when inside a flowsheet
-    #     assert heat_annual == pytest.approx(EXPECTED_HEAT_ENERGIES, 1e-3)
-    #     assert electricity_annual == pytest.approx(EXPECTED_ELECTRICITY_USE, 1e-3)
+        assert pytest.approx(499866550.0, rel=1e-3) == value(
+            m.fs.trough.costing.capital_cost
+        )
+        assert pytest.approx(2612089, rel=1e-3) == value(
+            m.fs.trough.costing.variable_operating_cost
+        )
+        assert pytest.approx(4000000.0, rel=1e-3) == value(
+            m.fs.trough.costing.fixed_operating_cost
+        )
+        assert pytest.approx(498620000.0, rel=1e-3) == value(
+            m.fs.trough.costing.direct_cost
+        )
