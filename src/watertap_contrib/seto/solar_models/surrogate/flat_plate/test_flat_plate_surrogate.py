b18d33667c481c34af81cd78b299de2d9fc3dce1
Matthew Boyd <mr.matthew.boyd@gmail.com>
2023-04-23 14:35:37 -0600

    Reformat flat plate tests to be similar to trough tests

--- src/watertap_contrib/seto/solar_models/surrogate/flat_plate/test_flat_plate_surrogate.py
+++ src/watertap_contrib/seto/solar_models/surrogate/flat_plate/test_flat_plate_surrogate.py
@@ -1,108 +1,127 @@
 import pytest
 import os
-import sys
-from io import StringIO
-from os.path import join, dirname, getsize
+
 import pandas as pd
-from pyomo.environ import ConcreteModel, assert_optimal_termination, Var, value
-from watertap_contrib.seto.solar_models.surrogate.flat_plate.data.training_flat_plate_surrogate import (
-    create_rbf_surrogate,
+from pyomo.environ import (
+    ConcreteModel,
+    SolverFactory,
+    Var,
+    value,
+    assert_optimal_termination,
 )
+from pyomo.network import Port
+
+from watertap_contrib.seto.solar_models.surrogate.flat_plate import FlatPlateSurrogate
 from watertap_contrib.seto.costing import EnergyCosting
+
 from idaes.core.solvers import get_solver
 from idaes.core.surrogate.pysmo_surrogate import PysmoSurrogate
 from idaes.core.surrogate.surrogate_block import SurrogateBlock
-from idaes.core import FlowsheetBlock
+from idaes.core import FlowsheetBlock, UnitModelCostingBlock
+from idaes.core.util.model_statistics import (
+    degrees_of_freedom,
+    number_variables,
+    number_total_constraints,
+    number_unused_variables,
+)
+from idaes.core.util.scaling import (
+    calculate_scaling_factors,
+    unscaled_variables_generator,
+)
 
-DATASET_FILENAME = join(dirname(__file__), "data/flat_plate_data.pkl")
-SURROGATE_FILENAME = join(dirname(__file__), "flat_plate_surrogate.json")
-N_SAMPLES = 100  # number of points to use from overall dataset
-TRAINING_FRACTION = 0.8
-INPUT_LABELS = ["heat_load", "hours_storage", "temperature_hot"]
-OUTPUT_LABELS = ["annual_energy", "electrical_load"]
-EXPECTED_HEAT_ENERGIES = [
-    2.215e9,
-    2.546e9,
+dataset_filename = os.path.join(os.path.dirname(__file__), "data/flat_plate_data.pkl")
+surrogate_filename = os.path.join(
+    os.path.dirname(__file__), "flat_plate_surrogate.json"
+)
+expected_heat_annual = [
+    2.201e9,
+    2.530e9,
     2.266e9,
-    1.478e9,
-    2.599e9,
-    3.544e8,
-    3.171e8,
-    2.206e9,
-    6.610e8,
-    1.137e9,
+    1.472e9,
+    2.611e9,
+    3.567e8,
+    3.049e8,
+    2.200e9,
+    6.599e8,
+    1.144e9,
+]
+expected_electricity_annual = [
+    4.889e7,
+    5.804e7,
+    4.969e7,
+    3.245e7,
+    5.682e7,
+    7.944e6,
+    6.800e6,
+    4.760e7,
+    1.530e7,
+    2.569e7,
 ]
 
-EXPECTED_ELECTRICITY_USE = [
-    4.866e7,
-    5.680e7,
-    4.930e7,
-    3.296e7,
-    5.717e7,
-    7.738e6,
-    7.517e6,
-    4.823e7,
-    1.532e7,
-    2.565e7,
-]
+
+def get_data():
+    df = pd.read_pickle(dataset_filename)
+    df = df.sample(n=90, random_state=1)  # random_state ensures reproducibility
+    return {"training": df[:80], "validation": df[80:90]}
 
 
 class TestFlatPlate:
     @pytest.fixture(scope="class")
-    def data(self):
-        # Read data manually for repeatability instead of using the training/validation function with random splits
-        df = pd.read_pickle(DATASET_FILENAME)
-        df = df.sample(n=90, random_state=1)  # random_state ensures reproducibility
-        return {"training": df[:80], "validation": df[80:90]}
-
-    @pytest.fixture(scope="class")
     def flat_plate_frame(self):
-        # create model and flowsheet
+
         m = ConcreteModel()
         m.fs = FlowsheetBlock(dynamic=False)
-
-        # add flowsheet input variables
-        m.fs.heat_load = Var(
-            initialize=1000, bounds=[100, 1000], doc="rated plant heat capacity in MWt"
-        )
-        m.fs.hours_storage = Var(
-            initialize=20, bounds=[0, 26], doc="rated plant hours of storage"
-        )
-        m.fs.temperature_hot = Var(
-            initialize=70, bounds=[50, 100], doc="hot outlet temperature"
-        )
-
-        # add flowsheet output variable
-        m.fs.heat_annual = Var(
-            initialize=5e9, doc="annual heat produced by the plant in kWht"
-        )
-        m.fs.electricity_annual = Var(
-            initialize=1e9, doc="annual electricity consumed by the plant in kWht"
-        )
-
-        # create input and output variable object lists for flowsheet
-        inputs = [m.fs.heat_load, m.fs.hours_storage, m.fs.temperature_hot]
-        outputs = [m.fs.heat_annual, m.fs.electricity_annual]
-
-        # capture long output
-        stream = StringIO()
-        oldstdout = sys.stdout
-        sys.stdout = stream
-
-        # load surrogate file and build model
-        m.fs.surrogate = SurrogateBlock(concrete=True)
-        surrogate = PysmoSurrogate.load_from_file(SURROGATE_FILENAME)
-        m.fs.surrogate.build_model(surrogate, input_vars=inputs, output_vars=outputs)
-
-        # revert back to standard output
-        sys.stdout = oldstdout
+        m.fs.flatplate = FlatPlateSurrogate()
 
         return m
 
     @pytest.mark.unit
-    def test_training(self, data):
-        """Test the creation and use of a new surrogate model"""
-        EXPECTED_HEAT_ENERGIES_TEST = [
+    def test_build(self, flat_plate_frame):
+        m = flat_plate_frame
+
+        assert len(m.fs.flatplate.config) == 3
+        assert not m.fs.flatplate.config.dynamic
+        assert not m.fs.flatplate.config.has_holdup
+        assert m.fs.flatplate._tech_type == "flat_plate"
+        assert isinstance(m.fs.flatplate.surrogate_blk, SurrogateBlock)
+
+        surr_input_str = ["heat_load", "hours_storage", "temperature_hot"]
+        surr_output_str = ["heat_annual", "electricity_annual"]
+
+        assert m.fs.flatplate.input_labels == surr_input_str
+        assert m.fs.flatplate.surrogate.input_labels() == surr_input_str
+        assert m.fs.flatplate.output_labels == surr_output_str
+        assert m.fs.flatplate.surrogate.output_labels() == surr_output_str
+        assert os.path.samefile(m.fs.flatplate.surrogate_file, surrogate_filename)
+        assert os.path.samefile(m.fs.flatplate.dataset_filename, dataset_filename)
+        assert m.fs.flatplate.surrogate.n_inputs() == 3
+        assert m.fs.flatplate.surrogate.n_outputs() == 2
+
+        for s in surr_input_str + surr_output_str:
+            v = getattr(m.fs.flatplate, s)
+            assert isinstance(v, Var)
+        assert m.fs.flatplate.n_samples == 100
+        assert m.fs.flatplate.training_fraction == 0.8
+
+        no_ports = list()
+        for c in m.fs.flatplate.component_objects():
+            if isinstance(c, Port):
+                no_ports.append(c)
+        assert len(no_ports) == 0
+        assert number_variables(m.fs.flatplate) == 7
+        assert number_unused_variables(m.fs.flatplate) == 0
+        assert number_total_constraints(m.fs.flatplate) == 4
+
+    @pytest.mark.unit
+    def test_surrogate_variable_bounds(self, flat_plate_frame):
+        m = flat_plate_frame
+        assert m.fs.flatplate.heat_load.bounds == tuple([100, 1000])
+        assert m.fs.flatplate.hours_storage.bounds == tuple([0, 26])
+        assert m.fs.flatplate.temperature_hot.bounds == tuple([50, 100])
+
+    @pytest.mark.component
+    def test_create_rbf_surrogate(self, flat_plate_frame):
+        expected_heat_annual_test = [
             2.206e9,
             2.556e9,
             2.267e9,
@@ -114,8 +133,7 @@ class TestFlatPlate:
             6.569e8,
             1.144e9,
         ]
-
-        EXPECTED_ELECTRICITY_USE_TEST = [
+        expected_electricity_annual_test = [
             4.918e7,
             5.697e7,
             5.019e7,
@@ -127,61 +145,91 @@ class TestFlatPlate:
             1.539e7,
             2.598e7,
         ]
-
-        # Create a new surrogate model and verify it saved to file
-        test_surrogate_filename = join(dirname(__file__), "test_surrogate.json")
-        test_surrogate = create_rbf_surrogate(
-            data["training"], INPUT_LABELS, OUTPUT_LABELS, test_surrogate_filename
+        m = flat_plate_frame
+        data = get_data()
+        test_surrogate_filename = os.path.join(
+            os.path.dirname(__file__), "test_surrogate.json"
         )
-        assert isinstance(test_surrogate, PysmoSurrogate)
-        assert getsize(test_surrogate_filename) > 1e4
+        m.fs.flatplate._create_rbf_surrogate(
+            bounds={"xmin": (100, 0, 50), "xmax": (1000, 26, 100)},
+            data_training=data["training"],
+            output_filename=test_surrogate_filename,
+        )
+        assert os.path.getsize(test_surrogate_filename) > 1e4
         os.remove(test_surrogate_filename)
-
-        # Verify surrogate model output is expected
-        output = test_surrogate.evaluate_surrogate(data["validation"])
-        assert list(output["annual_energy"]) == pytest.approx(
-            EXPECTED_HEAT_ENERGIES_TEST, 1e-3
+        assert isinstance(m.fs.flatplate.rbf_surr, PysmoSurrogate)
+        test_output = m.fs.flatplate.rbf_surr.evaluate_surrogate(data["validation"])
+        assert list(test_output["heat_annual"]) == pytest.approx(
+            expected_heat_annual_test, 1e-3
         )
-        assert list(output["electrical_load"]) == pytest.approx(
-            EXPECTED_ELECTRICITY_USE_TEST, 1e-3
+        assert list(test_output["electricity_annual"]) == pytest.approx(
+            expected_electricity_annual_test, 1e-3
         )
 
     @pytest.mark.component
-    def test_loading(self, data):
-        """Test the loading and use of the 'official' saved surrogate model"""
-        surrogate = PysmoSurrogate.load_from_file(SURROGATE_FILENAME)
-        assert isinstance(surrogate, PysmoSurrogate)
-        output = surrogate.evaluate_surrogate(data["validation"])
-        assert list(output["annual_energy"]) == pytest.approx(
-            EXPECTED_HEAT_ENERGIES, 1e-3
+    def test_validation(self, flat_plate_frame):
+
+        m = flat_plate_frame
+        data = get_data()
+        heat_annual_list = []
+        electricity_annual_list = []
+
+        for row in data["validation"].itertuples():
+            m.fs.flatplate.heat_load.fix(row.heat_load)
+            m.fs.flatplate.hours_storage.fix(row.hours_storage)
+            m.fs.flatplate.temperature_hot.fix(row.temperature_hot)
+            solver = SolverFactory("ipopt")
+            results = solver.solve(m)
+            assert_optimal_termination(results)
+            heat_annual_list.append(value(m.fs.flatplate.heat_annual))
+            electricity_annual_list.append(value(m.fs.flatplate.electricity_annual))
+
+        # ensure surrogate model gives same results when inside a flowsheet
+        assert heat_annual_list == pytest.approx(expected_heat_annual, 1e-3)
+        assert electricity_annual_list == pytest.approx(
+            expected_electricity_annual, 1e-3
         )
-        assert list(output["electrical_load"]) == pytest.approx(
-            EXPECTED_ELECTRICITY_USE, 1e-3
-        )
+
+    @pytest.mark.unit
+    def test_dof(self, flat_plate_frame):
+
+        m = flat_plate_frame
+        m.fs.flatplate.heat_load.fix(500)
+        m.fs.flatplate.hours_storage.fix(12)
+        m.fs.flatplate.temperature_hot.fix(70)
+        assert degrees_of_freedom(m) == 0
+
+    @pytest.mark.unit
+    def test_calculate_scaling(self, flat_plate_frame):
+
+        m = flat_plate_frame
+        calculate_scaling_factors(m)
+        assert len(list(unscaled_variables_generator(m))) == 0
 
     @pytest.mark.component
     def test_costing(self, flat_plate_frame):
         m = flat_plate_frame
         m.fs.costing = EnergyCosting()
+        m.fs.flatplate.costing = UnitModelCostingBlock(
+            flowsheet_costing_block=m.fs.costing
+        )
+        m.fs.costing.factor_maintenance_labor_chemical.fix(0)
+        m.fs.costing.factor_total_investment.fix(1)
+        m.fs.costing.cost_process()
 
-    @pytest.mark.component
-    def test_flowsheet_use(self, data, flat_plate_frame):
-        """Test creating a flowsheet using the surrogate model"""
-        m = flat_plate_frame
-        heat_annual = []
-        electricity_annual = []
+        solver = SolverFactory("ipopt")
+        results = solver.solve(m)
+        assert_optimal_termination(results)
 
-        # evaluate flowsheet with surrogate model using validation data as input
-        for row in data["validation"].itertuples():
-            m.fs.heat_load.fix(row.heat_load)
-            m.fs.hours_storage.fix(row.hours_storage)
-            m.fs.temperature_hot.fix(row.temperature_hot)
-            solver = get_solver()
-            results = solver.solve(m)
-            assert_optimal_termination(results)
-            heat_annual.append(value(m.fs.heat_annual))
-            electricity_annual.append(value(m.fs.electricity_annual))
-
-        # ensure surrogate model gives same results when inside a flowsheet
-        assert heat_annual == pytest.approx(EXPECTED_HEAT_ENERGIES, 1e-3)
-        assert electricity_annual == pytest.approx(EXPECTED_ELECTRICITY_USE, 1e-3)
+        assert pytest.approx(240938.5, rel=1e-3) == value(
+            m.fs.flatplate.costing.capital_cost
+        )
+        assert pytest.approx(0, rel=1e-3) == value(
+            m.fs.flatplate.costing.variable_operating_cost
+        )
+        assert pytest.approx(8800000.1, rel=1e-3) == value(
+            m.fs.flatplate.costing.fixed_operating_cost
+        )
+        assert pytest.approx(229465.3, rel=1e-3) == value(
+            m.fs.flatplate.costing.direct_cost
+        )
