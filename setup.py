bb3f93005792001664c5b6c2f00783f53ecd1ece
Zhuoran Zhang <zz2322@columbia.edu>
2023-06-15 14:36:18 -0400

    specify pyomo version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
-        # "pyomo <= 6.5",
+        "pyomo == 6.6.1",
         "pytest >= 7",
         "nrel-pysam == 3.0.2",
     ],
