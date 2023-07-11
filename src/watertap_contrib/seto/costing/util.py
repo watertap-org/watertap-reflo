a72e70cb1709512c86dc61d1d3996ec1647a45e4
kurbansitterley <kurban.sitterley@nrel.gov>
2023-07-10 20:52:25 -0600

    chagne bounds for util Vars

--- src/watertap_contrib/seto/costing/util.py
+++ src/watertap_contrib/seto/costing/util.py
@@ -4,7 +4,7 @@ import pyomo.environ as pyo
 def make_capital_cost_var(blk):
     blk.capital_cost = pyo.Var(
         initialize=1e5,
-        # domain=pyo.NonNegativeReals,
+        bounds=(0, None),
         units=blk.costing_package.base_currency,
         doc="Unit capital cost",
     )
@@ -13,7 +13,7 @@ def make_capital_cost_var(blk):
 def make_variable_operating_cost_var(blk):
     blk.variable_operating_cost = pyo.Var(
         initialize=1e5,
-        # domain=pyo.NonNegativeReals,
+        bounds=(0, None),
         units=blk.costing_package.base_currency / blk.costing_package.base_period,
         doc="Unit variable operating cost",
     )
@@ -22,7 +22,7 @@ def make_variable_operating_cost_var(blk):
 def make_fixed_operating_cost_var(blk):
     blk.fixed_operating_cost = pyo.Var(
         initialize=1e5,
-        # domain=pyo.NonNegativeReals,
+        bounds=(0, None),
         units=blk.costing_package.base_currency / blk.costing_package.base_period,
         doc="Unit fixed operating cost",
     )
