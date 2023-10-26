a0b2589196aa99477a6731b095d4175ff7c16bd2
Adam Atia <aatia@keylogic.com>
2023-10-26 19:24:04 -0400

    try different wt version

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author="WaterTAP-SETO contributors",
     python_requires=">=3.8",
     install_requires=[
-        "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
-        # "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
+        # "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
+        "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
         # "watertap <= 0.8",
         "pyomo <= 6.5",
         "pytest >= 7",
@@ -20,7 +20,7 @@ setup(
             "nbsphinx",  # jupyter notebook support for sphinx
             "jinja2<3.1.0",  # see watertap-org/watertap#449
             "Sphinx==7.1.*",  # docs
-            "sphinx_rtd_theme",  # docs
+            "sphinx-rtd-theme",  # docs
         ]
     },
 )
