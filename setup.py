3e3155fee6739a4a585cbca5424a7f54a0bfe123
Adam Atia <aatia@keylogic.com>
2023-10-26 19:26:21 -0400

    watertap 0.8

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         # "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
-        "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
-        # "watertap <= 0.8",
+        # "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
+        "watertap <= 0.8",
         "pyomo <= 6.5",
         "pytest >= 7",
         "nrel-pysam == 3.0.2",
