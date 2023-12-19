7eda1ae67abc07d916987ea13b1a937484b535be
kurbansitterley <kurban.sitterley@nrel.gov>
2023-12-19 14:50:12 -0700

    setup point to watertap >= 0.10

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ setup(
     author="WaterTAP-REFLO contributors",
     python_requires=">=3.8",
     install_requires=[
-        "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
+        # "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
         # "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
-        # "watertap <= 0.8",
-        "pyomo <= 6.5",
+        "watertap >= 0.10",
+        # "pyomo <= 6.5",
         "pytest >= 7",
         "nrel-pysam == 3.0.2",
     ],
