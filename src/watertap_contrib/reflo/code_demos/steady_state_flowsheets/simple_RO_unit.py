cc3f960f57e31c27b7649fa8bda6811a2a7eb3b2
Zach Binger <zbinger@nrel.gov>
2023-12-19 17:07:11 -0700

    ran black

--- src/watertap_contrib/reflo/code_demos/steady_state_flowsheets/simple_RO_unit.py
+++ src/watertap_contrib/reflo/code_demos/steady_state_flowsheets/simple_RO_unit.py
@@ -1,5 +1,4 @@
 class ROUnit:
     def __init__(self):
-        self.product = 6000 # m3/day
-        self.power_demand = 1000 # kW
-
+        self.product = 6000  # m3/day
+        self.power_demand = 1000  # kW
