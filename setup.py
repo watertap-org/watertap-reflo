db13650708c77b06bfb901bd89a8fa72637cac63
kurbansitterley <kurban.sitterley@nrel.gov>
2023-12-22 14:03:31 -0700

    clean up setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,7 @@ setup(
     author="WaterTAP-REFLO contributors",
     python_requires=">=3.8",
     install_requires=[
-        # "watertap @ https://github.com/watertap-org/watertap/archive/refs/tags/pr967.zip",
-        # "watertap @ https://github.com/watertap-org/watertap/archive/main.zip",
         "watertap == 0.11.0rc1",
-        # "pyomo >= 6.6.2",
-        # "pyomo <= 6.5",
-        # "pytest >= 7",
         "nrel-pysam == 3.0.2",
     ],
     extras_require={
