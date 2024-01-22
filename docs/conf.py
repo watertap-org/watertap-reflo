c6636a95b4a18f2d837417e738f63f73e26bcaa9
Zach Binger <zbinger@nrel.gov>
2024-01-22 16:47:40 -0700

    updated logo

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ html_css_files = ["custom.css"]
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = "_static/WaterTAP_REFLO.png"
+html_logo = "_static/WaterTAP-REFLO.png"
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
