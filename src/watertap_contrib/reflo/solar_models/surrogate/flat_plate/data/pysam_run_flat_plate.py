a485007931c18b1b50b05b1cbac86ebea8d7c8fa
Darice <darice.guittet@nrel.gov>
2024-02-06 09:42:48 -0700

    clean up code

--- src/watertap_contrib/reflo/solar_models/surrogate/flat_plate/data/pysam_run_flat_plate.py
+++ src/watertap_contrib/reflo/solar_models/surrogate/flat_plate/data/pysam_run_flat_plate.py
@@ -93,9 +93,8 @@ def setup_model(
         raise Exception("Either weather_file or weather_data must be specified.")
 
     # Set constant temperatures
-    tech_model.value("use_custom_mains", 1)
     tech_model.value("custom_mains", 8760 * (temperatures["T_cold"],))
-    tech_model.value("use_custom_set", 0)  # ensure constant hot set temperature is used
+
     tech_model.value("T_set", temperatures["T_hot"])
     tech_model.value("T_room", temperatures["T_amb"])
 
@@ -103,10 +102,6 @@ def setup_model(
     tech_model.value(
         "mdot", tech_model.value("test_flow") * tech_model.value("ncoll")
     )  # [kg/s]
-
-    # Set pump and piping parameters
-    tech_model.value("pipe_diam", 0.019)  # [m] default is 0.019 m
-    tech_model.value("pump_eff", 0.85)  # [-] pump efficiency default is 0.85
 
     # Ensure system capacity parameter agreement
     system_capacity_actual = system_capacity_computed(tech_model)
