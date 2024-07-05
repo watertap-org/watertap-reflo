15c0c41de94c33691c942a73f3354558ebc4811f
kurbansitterley <kurban.sitterley@nrel.gov>
2024-07-05 15:26:40 -0600

    clean up imports; use correct import

--- src/watertap_contrib/reflo/analysis/multiperiod/vagmd_batch/test/test_VAGMD_batch_flowsheet_multiperiod.py
+++ src/watertap_contrib/reflo/analysis/multiperiod/vagmd_batch/test/test_VAGMD_batch_flowsheet_multiperiod.py
@@ -19,13 +19,6 @@ from pyomo.environ import (
 )
 
 from idaes.core import FlowsheetBlock
-from watertap_contrib.reflo.analysis.multiperiod.vagmd_batch.VAGMD_batch_multiperiod_unit_model import (
-    VAGMDbatchSurrogate,
-)
-
-from watertap_contrib.reflo.costing import REFLOCosting
-
-from idaes.core.solvers import get_solver
 from idaes.core.util.exceptions import (
     ConfigurationError,
 )
@@ -36,7 +29,7 @@ from idaes.core.util.model_statistics import (
 from watertap.core.solvers import get_solver
 
 from watertap_contrib.reflo.costing import REFLOCosting
-from watertap_contrib.reflo.analysis.multiperiod.vagmd_batch.VAGMD_batch_flowsheet_multiperiod import (
+from watertap_contrib.reflo.analysis.multiperiod.vagmd_batch.VAGMD_batch_multiperiod_unit_model import (
     VAGMDbatchSurrogate,
 )
 
