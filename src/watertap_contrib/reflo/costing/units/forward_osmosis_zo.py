dd1a334dca1eb9fed485c091df44c1e4bb506ae8
kurbansitterley <kurban.sitterley@nrel.gov>
2024-09-11 13:15:00 -0600

    unused import

--- src/watertap_contrib/reflo/costing/units/forward_osmosis_zo.py
+++ src/watertap_contrib/reflo/costing/units/forward_osmosis_zo.py
@@ -11,7 +11,6 @@
 #################################################################################
 
 import pyomo.environ as pyo
-import idaes.core.util.scaling as iscale
 from watertap.costing.util import register_costing_parameter_block
 from watertap_contrib.reflo.costing.util import (
     make_capital_cost_var,
