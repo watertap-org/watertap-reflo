826160348a08459cce4d4a5a4b2c169da70e34a6
kurbansitterley <kurban.sitterley@nrel.gov>
2024-09-24 12:55:40 -0600

    add all params for capex

--- src/watertap_contrib/reflo/costing/units/deep_well_injection.py
+++ src/watertap_contrib/reflo/costing/units/deep_well_injection.py
@@ -1,5 +1,5 @@
 #################################################################################
-# WaterTAP Copyright (c) 2020-2023, The Regents of the University of California,
+# WaterTAP Copyright (c) 2020-2024, The Regents of the University of California,
 # through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
 # National Renewable Energy Laboratory, and National Energy Technology
 # Laboratory (subject to receipt of any required approvals from the U.S. Dept.
@@ -34,6 +34,119 @@ def build_deep_well_injection_cost_param_block(blk):
     """
     costing = blk.parent_block()
 
+    # All default values are for 2,500 ft depth
+
+    blk.logging_testing_capital_cost_intercept = Param(
+        initialize=251.48,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Logging + testing capital cost equation - intercept",
+    )
+
+    blk.logging_testing_capital_cost_slope = Param(
+        initialize=4.5193,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.inches**-1,
+        doc="Logging + testing capital cost equation - slope",
+    )
+
+    blk.drilling_capital_cost_intercept = Param(
+        initialize=338.05,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Drilling capital cost equation - intercept",
+    )
+
+    blk.drilling_capital_cost_slope = Param(
+        initialize=17.611,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.inches**-1,
+        doc="Drilling capital cost equation - slope",
+    )
+
+    blk.tubing_capital_cost_base = Param(
+        initialize=116.65,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.inches**-1,
+        doc="Tubing capital cost equation - base",
+    )
+
+    blk.tubing_capital_cost_exponent = Param(
+        initialize=0.3786,
+        mutable=True,
+        units=pyunits.dimensionless,
+        doc="Tubing capital cost equation - exponent",
+    )
+
+    blk.packing_capital_cost_base = Param(
+        initialize=579.85,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Packing capital cost equation - base",
+    )
+
+    blk.packing_capital_cost_intercept = Param(
+        initialize=596.64,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Packing capital cost equation - intercept",
+    )
+
+    blk.casing_capital_cost_intercept = Param(
+        initialize=20.545,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Casing capital cost equation - intercept",
+    )
+
+    blk.casing_capital_cost_slope = Param(
+        initialize=384.36,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.inches**-1,
+        doc="Casing capital cost equation - slope",
+    )
+
+    blk.grouting_capital_cost_base = Param(
+        initialize=145.01,
+        mutable=True,
+        units=pyunits.kUSD_2001,
+        doc="Grouting capital cost equation - base",
+    )
+
+    blk.grouting_capital_cost_exp_coeff = Param(
+        initialize=0.0708,
+        mutable=True,
+        units=pyunits.inches**-1,
+        doc="Grouting capital cost equation - exponential coefficient",
+    )
+
+    blk.monitoring_well_capital_cost_base = Param(
+        initialize=31.337,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.feet**-1,
+        doc="Monitoring well capital cost equation - base",
+    )
+
+    blk.monitoring_well_capital_cost_exponent = Param(
+        initialize=0.3742,
+        mutable=True,
+        units=pyunits.dimensionless,
+        doc="Monitoring well capital cost equation - exponent",
+    )
+
+    blk.mobilization_capital_cost_base = Param(
+        initialize=31.337,
+        mutable=True,
+        units=pyunits.kUSD_2001 * pyunits.feet**-1,
+        doc="Mobilization capital cost equation - base",
+    )
+
+    blk.mobilization_capital_cost_exponent = Param(
+        initialize=0.3742,
+        mutable=True,
+        units=pyunits.dimensionless,
+        doc="Mobilization capital cost equation - exponent",
+    )
 
 
 @register_costing_parameter_block(
@@ -44,6 +157,79 @@ def cost_deep_well_injection(blk):
     """
     Capital and operating costs for deep well injection
     """
-    dwi = blk.costing_package.deep_well_injection
+    dwi_params = blk.costing_package.deep_well_injection
+    dwi = blk.unit_model
     make_capital_cost_var(blk)
-    make_fixed_operating_cost_var(blk)
+    # make_fixed_operating_cost_var(blk)
+
+    blk.logging_testing_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Logging + testing capital costs",
+    )
+
+    blk.drilling_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Drilling capital costs",
+    )
+
+    blk.tubing_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Tubing capital costs",
+    )
+
+    blk.packing_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Packing capital costs",
+    )
+
+    blk.casing_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Casing capital costs",
+    )
+
+    blk.grouting_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Grouting capital costs",
+    )
+
+    blk.monitoring_well_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Monitoring well capital costs",
+    )
+
+    blk.mobilization_capital_cost = Var(
+        initialize=1e5,
+        domain=NonNegativeReals,
+        units=blk.costing_package.base_currency,
+        doc="Mobilization capital costs",
+    )
+
+    capital_cost_expr = 0
+
+    blk.logging_testing_capital_cost_constraint = Constraint(
+        expr=blk.logging_testing_capital_cost
+        == pyunits.convert(
+            dwi_params.logging_testing_capital_cost_slope * dwi.pipe_diameter
+            + dwi_params.logging_testing_capital_cost_intercept,
+            to_units=blk.costing_package.base_currency,
+        )
+    )
+
+    capital_cost_expr += blk.logging_testing_capital_cost
+
+    blk.costing_package.add_cost_factor(blk, None)
+    blk.capital_cost_constraint = Constraint(expr=blk.capital_cost == capital_cost_expr)
