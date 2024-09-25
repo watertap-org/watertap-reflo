1aa52ac0107bcbbef3a5f7c90e564a79d7441d08
kurbansitterley <kurban.sitterley@nrel.gov>
2024-09-25 12:19:35 -0600

    clean up; remove __main__ func

--- src/watertap_contrib/reflo/unit_models/deep_well_injection.py
+++ src/watertap_contrib/reflo/unit_models/deep_well_injection.py
@@ -1,5 +1,5 @@
 #################################################################################
-# WaterTAP Copyright (c) 2020-2023, The Regents of the University of California,
+# WaterTAP Copyright (c) 2020-2024, The Regents of the University of California,
 # through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
 # National Renewable Energy Laboratory, and National Energy Technology
 # Laboratory (subject to receipt of any required approvals from the U.S. Dept.
@@ -10,14 +10,11 @@
 # "https://github.com/watertap-org/watertap/"
 #################################################################################
 
-from copy import deepcopy
 from pyomo.environ import (
-    Var,
     check_optimal_termination,
     Param,
     Suffix,
     units as pyunits,
-    Expr_if,
 )
 from pyomo.common.config import ConfigBlock, ConfigValue, In
 
@@ -30,9 +27,7 @@ from idaes.core import (
 from idaes.core.util.math import smooth_bound
 from idaes.core.util.tables import create_stream_table_dataframe
 from idaes.core.util.config import is_physical_parameter_block
-from idaes.core.util.misc import StrEnum
 from idaes.core.util.exceptions import InitializationError
-import idaes.core.util.scaling as iscale
 import idaes.logger as idaeslog
 
 from watertap.core import InitializationMixin
@@ -43,20 +38,12 @@ from watertap_contrib.reflo.costing.units.deep_well_injection import (
 
 __author__ = "Kurban Sitterley"
 
-_log = idaeslog.getLogger(__name__)
-
-
-class SofteningProcedureType(StrEnum):
-    single_stage_lime = "single_stage_lime"
-    excess_lime = "excess_lime"
-    single_stage_lime_soda = "single_stage_lime_soda"
-    excess_lime_soda = "excess_lime_soda"
-
 
 @declare_process_block_class("DeepWellInjection")
 class DeepWellInjectionData(InitializationMixin, UnitModelBlockData):
     """
-    Zero deep well injection model
+    Zero order deep well injection model.
+    This is a terminal unit and has no outlet.
     """
 
     CONFIG = ConfigBlock()
@@ -116,7 +103,7 @@ class DeepWellInjectionData(InitializationMixin, UnitModelBlockData):
             default=2500,
             domain=In([2500, 5000, 7500, 10000]),
             description="Depth of injection well. Costing is available for 2500, 5000, 7500, and 10000 ft depths.",
-            doc="""Depth of injection well for costing purposes.""",
+            doc="""Depth of injection well for use with BLM costing approach.""",
         ),
     )
 
@@ -240,139 +227,3 @@ class DeepWellInjectionData(InitializationMixin, UnitModelBlockData):
     @property
     def default_costing_method(self):
         return cost_deep_well_injection
-
-
-if __name__ == "__main__":
-
-    from pyomo.environ import (
-        ConcreteModel,
-        Expression,
-        TerminationCondition,
-        SolverStatus,
-        value,
-        Var,
-        assert_optimal_termination,
-        units as pyunits,
-    )
-    from pyomo.network import Port
-    from idaes.core import FlowsheetBlock
-    from pyomo.util.check_units import assert_units_consistent
-
-    from watertap.core.solvers import get_solver
-
-    from watertap.unit_models.tests.unit_test_harness import UnitTestHarness
-    from idaes.core.util.model_statistics import (
-        degrees_of_freedom,
-        number_variables,
-        number_total_constraints,
-        number_unused_variables,
-    )
-    from idaes.core.util.testing import initialization_tester
-    from idaes.core.util.scaling import (
-        calculate_scaling_factors,
-        unscaled_variables_generator,
-        badly_scaled_var_generator,
-    )
-    from idaes.core import UnitModelCostingBlock, MaterialFlowBasis
-
-    from watertap.property_models.multicomp_aq_sol_prop_pack import (
-        MCASParameterBlock,
-    )
-    from watertap_contrib.reflo.costing import TreatmentCosting
-
-    from pprint import pprint
-    from watertap_contrib.reflo.costing import TreatmentCosting
-    import watertap.property_models.unit_specific.cryst_prop_pack as props
-    from watertap.property_models.water_prop_pack import WaterParameterBlock
-    from watertap.core.util.model_diagnostics.infeasible import *
-    from pyomo.util.calc_var_value import calculate_variable_from_constraint as cvc
-    from idaes.core.util.scaling import *
-    from idaes.core.util.testing import initialization_tester
-    from idaes.core.util.scaling import (
-        calculate_scaling_factors,
-        unscaled_variables_generator,
-        badly_scaled_var_generator,
-    )
-    from watertap_contrib.reflo.kurby import print_unit_solutions, make_test_dict
-
-    solver = get_solver()
-
-    inlet_conc = {
-        "Ca_2+": 1.43,
-        "Mg_2+": 0.1814,
-        "SiO2": 0.054,
-        "Alkalinity_2-": 0.421,
-    }
-
-    flow_mgd = 5.08 * pyunits.Mgallons / pyunits.day
-    # flow_mgd = 0.95 * pyunits.Mgallons / pyunits.day
-    rho = 1000 * pyunits.kg / pyunits.m**3
-
-    flow_mass_water_in = pyunits.convert(
-        flow_mgd * rho, to_units=pyunits.kg / pyunits.s
-    )
-
-    m = ConcreteModel()
-    m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.properties = MCASParameterBlock(
-        solute_list=inlet_conc.keys(), material_flow_basis=MaterialFlowBasis.mass
-    )
-    m.fs.dwi = dwi = DeepWellInjection(
-        property_package=m.fs.properties, injection_well_depth=2500
-    )
-    m.fs.costing = TreatmentCosting()
-    # m.fs.costing.base_currency = pyunits.kUSD_2001
-    m.fs.dwi.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
-    m.fs.costing.cost_process()
-    m.fs.costing.add_LCOW(dwi.properties[0].flow_vol_phase["Liq"])
-    prop_in = dwi.properties[0]
-    prop_in.conc_mass_phase_comp
-    prop_in.temperature.fix()
-    prop_in.pressure.fix()
-    flow_mass_phase_water = pyunits.convert(
-        flow_mgd * rho, to_units=pyunits.kg / pyunits.s
-    )
-    prop_in.flow_mass_phase_comp["Liq", "H2O"].fix(flow_mass_phase_water)
-    for solute, conc in inlet_conc.items():
-        mass_flow_solute = pyunits.convert(
-            flow_mgd * conc * pyunits.kg / pyunits.m**3,
-            to_units=pyunits.kg / pyunits.s,
-        )
-        prop_in.flow_mass_phase_comp["Liq", solute].fix(mass_flow_solute)
-        m.fs.properties.set_default_scaling(
-            "flow_mass_phase_comp",
-            value(1 / mass_flow_solute),
-            index=("Liq", solute),
-        )
-    m.fs.properties.set_default_scaling(
-        "flow_mass_phase_comp",
-        value(1 / flow_mass_phase_water),
-        index=("Liq", "H2O"),
-    )
-
-    dwi.injection_pressure.set_value(1)
-    dwi.initialize()
-
-    results = solver.solve(m)
-
-    print(
-        f"pipe_diameter = {dwi.pipe_diameter()} {pyunits.get_units(dwi.pipe_diameter)}"
-    )
-    print(f"well_depth = {dwi.injection_well_depth()} ft")
-    print()
-    print(f"DOF = {degrees_of_freedom(m)}")
-    dwi.properties[0].display()
-    assert False
-    print(f"LCOW = {m.fs.costing.LCOW()}")
-    # print(
-    #     dwi.flow_mgd(),
-    #     flow_mass_water_in(),
-    #     dwi.properties[0].flow_mass_phase_comp["Liq", "H2O"](),
-    #     dwi.properties[0].flow_vol_phase["Liq"](),
-    # )
-    # dwi.well_depth.display()
-
-    dwi.costing.display()
-    m.fs.costing.total_operating_cost.display()
-    m.fs.costing.total_fixed_operating_cost.display()
-    m.fs.costing.total_variable_operating_cost.display()
