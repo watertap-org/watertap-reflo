132e30b7557531cd9d48485fa144a5c9fbc190f0
kurbansitterley <kurban.sitterley@nrel.gov>
2024-10-28 09:34:49 -0400

    add translators; make importable

--- src/watertap_contrib/reflo/analysis/case_studies/permian/components/__init__.py
+++ src/watertap_contrib/reflo/analysis/case_studies/permian/components/__init__.py
@@ -2,3 +2,7 @@ from .cartridge_filtration import *
 from .EC import *
 from .oxidation import *
 from .MVC import *
+from .deep_well_injection import *
+
+from .translator_zo_to_sw import Translator_ZO_to_SW
+from .translator_sw_to_zo import Translator_SW_to_ZO
