905c024f44af6bc97525704f94b713e1087f5460
kurbansitterley <kurban.sitterley@nrel.gov>
2024-11-02 12:12:28 -0400

    sw to water tb

--- src/watertap_contrib/reflo/analysis/case_studies/permian/components/__init__.py
+++ src/watertap_contrib/reflo/analysis/case_studies/permian/components/__init__.py
@@ -6,3 +6,4 @@ from .deep_well_injection import *
 
 from .translator_zo_to_sw import Translator_ZO_to_SW
 from .translator_sw_to_zo import Translator_SW_to_ZO
+from .translator_sw_to_water import Translator_SW_to_Water
