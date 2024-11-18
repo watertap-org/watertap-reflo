f9afc04eb9a6706bec5b3097daf79ebe58acc512
kurbansitterley <kurban.sitterley@nrel.gov>
2024-11-18 11:57:08 -0700

    updated for new costing and water yield calc

--- src/watertap_contrib/reflo/unit_models/tests/test_solar_still.py
+++ src/watertap_contrib/reflo/unit_models/tests/test_solar_still.py
@@ -60,24 +60,16 @@ inlet_dict = {
     "material_flow_basis": MaterialFlowBasis.mass,
 }
 
-# water_yield_calc_dict = dict(
-#     input_weather_file_path=test_data_path,  # path to input weather file
-#     irradiance_threshold=0,  # irradiance values < threshold assumed to have negligible impact on calculation; W/m2
-#     salinity=200,  # salinity of influent water; g/L
-#     water_depth_basin=0.01,  # depth of water in solar still basin; m
-#     length_basin=0.6,  # length of each side of basin (length=width); m
-# )
-
 water_yield_calc_dict = dict(
-    input_weather_file_path="/Users/ksitterl/Documents/Python/watertap-reflo/watertap-reflo/src/watertap_contrib/reflo/unit_models/tests/solar_still_test_data_phoenix_az.csv",  # path to input weather file
-    initial_salinity=200,  # initial salinity of influent water; g/L
+    input_weather_file_path=test_data_path,  # path to input weather file
+    initial_salinity=120,  # initial salinity of influent water; g/L
     initial_water_depth=0.01,  # initial depth of water in solar still basin; m
     length_basin=0.6,  # length of each side of basin (length=width); m
     irradiance_threshold=20,  # irradiance values < threshold assumed to have negligible impact on calculation; W/m2
 )
 
-tds_conc = 200 * pyunits.g / pyunits.liter
-daily_water_production = 100 * pyunits.m**3 / pyunits.day
+tds_conc = 120 * pyunits.g / pyunits.liter
+daily_water_production = 200 * pyunits.m**3 / pyunits.day
 
 flow_mass_in = pyunits.convert(
     daily_water_production * rho, to_units=pyunits.kg / pyunits.s
@@ -178,17 +170,18 @@ class TestSolarStill:
         m = ss_frame
 
         results_dict = {
-            "water_yield": 1.701793,
+            "number_zld_cycles": 65.5,
+            "water_yield": 1.7552,
             "length_basin": 0.6,
             "dens_mass_salt": 2.16,
-            "number_stills": 163226.45,
-            "total_area": 58761.522,
-            "annual_water_yield": 0.621154772,
-            "flow_vol_salt": 0.000107167,
-            "deposition_rate": 0.01575735,
+            "number_stills": 316508.0,
+            "total_area": 113942.0,
+            "annual_water_yield": 0.6406,
+            "flow_vol_salt": 0.0001286,
+            "deposition_rate": 0.009751471,
             "area_single_still": 0.36,
-            "yield_per_still": 7.09e-06,
-            "evaporation_rate": 1.701793,
+            "yield_per_still": 7.313e-06,
+            "evaporation_rate": 1.7552,
         }
         for v, r in results_dict.items():
             ssv = getattr(m.fs.unit, v)
@@ -213,19 +206,19 @@ class TestSolarStill:
         assert_optimal_termination(results)
 
         sys_cost_results = {
-            "aggregate_capital_cost": 3654601.821,
-            "aggregate_fixed_operating_cost": 127911.063,
-            # "aggregate_variable_operating_cost": 0.0,
+            "aggregate_capital_cost": 10758558.1,
+            "aggregate_fixed_operating_cost": 376549.5,
+            "aggregate_variable_operating_cost": 0.0,
             "aggregate_flow_electricity": 2.625,
-            # "aggregate_flow_costs": {"electricity": 0.0},
-            "aggregate_direct_capital_cost": 1827300.91,
-            "total_capital_cost": 3654601.821,
-            "total_operating_cost": 237549.118,
-            "maintenance_labor_chemical_operating_cost": 109638.054,
-            "total_fixed_operating_cost": 237549.118,
-            # "total_variable_operating_cost": 0.0,
-            "total_annualized_cost": 646703.534,
-            "LCOW": 17.705,
+            "aggregate_flow_costs": {"electricity": 1890.9},
+            "aggregate_direct_capital_cost": 10758558.1,
+            "total_capital_cost": 10758558.1,
+            "total_operating_cost": 701197.1,
+            "maintenance_labor_chemical_operating_cost": 322756.7,
+            "total_fixed_operating_cost": 699306.3,
+            "total_variable_operating_cost": 1890.91,
+            "total_annualized_cost": 1905681.8,
+            "LCOW": 26.087,
         }
 
         for v, r in sys_cost_results.items():
@@ -237,21 +230,19 @@ class TestSolarStill:
                 assert pytest.approx(value(sc), rel=1e-3) == r
 
         ss_cost_results = {
-            "capital_cost": 3654601.821,
-            "fixed_operating_cost": 127911.063,
-            "max_sw_flow": 1728.0,
-            "max_fw_flow": 86.4,
-            "pumping_power_required": 2.625,
-            "length_piping": 481967.866,
-            "capital_cost_solar_still": 2791221.587,
+            "capital_cost": 10758558.1,
+            "fixed_operating_cost": 376549.5,
+            "capital_cost_per_still": 28.716,
+            "length_piping": 934571.157,
+            "capital_cost_solar_still": 9088887.7,
             "capital_cost_sw_pumps": 890.64,
             "capital_cost_fw_pumps": 178.128,
-            "capital_cost_feed_tank": 1403.673,
-            "capital_cost_distillate_tank": 1493.892,
-            "capital_cost_underground_tank": 323.247,
-            "capital_cost_excavation": 570.232,
-            "capital_cost_piping": 858520.419,
-            "direct_capital_cost": 1827300.91,
+            "capital_cost_feed_tank": 1431.622,
+            "capital_cost_distillate_tank": 1522.2,
+            "capital_cost_underground_tank": 332.05,
+            "capital_cost_excavation": 581.34,
+            "capital_cost_piping": 1664734.26,
+            "direct_capital_cost": 10758558.1,
         }
 
         for v, r in ss_cost_results.items():
@@ -274,7 +265,7 @@ def test_input_data_too_short():
     with pytest.raises(ValueError):
         err_msg = f"Water yield calculation for fs\\.unit requires at least "
         err_msg += f"one year of hourly weather data, but the input dataset is "
-        err_msg += f"1720 hours long\\."
+        err_msg += f"only 1720 hours long\\."
         m.fs.unit = SolarStill(
             property_package=m.fs.properties,
             water_yield_calculation_args=water_yield_calc_dict,
