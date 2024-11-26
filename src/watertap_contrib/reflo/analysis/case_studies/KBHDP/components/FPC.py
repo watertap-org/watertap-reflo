c60fb28cbbc25180b7ddbb1ea4f236621822239a
zacharybinger <zbinger@nrel.gov>
2024-11-26 11:32:44 -0700

    black

--- src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/FPC.py
+++ src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/FPC.py
@@ -67,9 +67,7 @@ def build_fpc(m):
 
     file = cwd + "/src/watertap_contrib/reflo/solar_models/surrogate/flat_plate/"
 
-    dataset_filename = os.path.join(
-        os.path.dirname(file), "data/FPC_Heat_Load.pkl"
-    )
+    dataset_filename = os.path.join(os.path.dirname(file), "data/FPC_Heat_Load.pkl")
     surrogate_filename = os.path.join(
         os.path.dirname(file),
         "data/flat_plate_data_heat_load_1_400_heat_load_1_400_hours_storage_0_27_temperature_hot_50_102.json",
