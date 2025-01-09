6d728e66e749db503a8c69499c28b896214c6592
kurbansitterley <kurban.sitterley@nrel.gov>
2025-01-09 11:40:09 -0700

    clean up

--- src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/multi_effect_crystallizer.py
+++ src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/multi_effect_crystallizer.py
@@ -1,83 +1,33 @@
-import os
-import math
-import numpy as np
 from pyomo.environ import (
     ConcreteModel,
-    value,
-    Param,
-    Var,
-    Constraint,
-    Set,
-    Expression,
-    Objective,
-    NonNegativeReals,
     TransformationFactory,
-    Block,
-    RangeSet,
     check_optimal_termination,
     assert_optimal_termination,
+    value,
     units as pyunits,
 )
-from pyomo.network import Arc, SequentialDecomposition
-from pyomo.util.check_units import assert_units_consistent
+from pyomo.network import Arc
+
+from idaes.models.unit_models import Product, Feed, StateJunction
 from idaes.core import FlowsheetBlock, UnitModelCostingBlock
 from idaes.core.solvers import get_solver
 from idaes.core.util.initialization import propagate_state
-import idaes.core.util.scaling as iscale
+from idaes.core.util.model_statistics import *
 from idaes.core.util.scaling import (
     constraint_scaling_transform,
     calculate_scaling_factors,
     set_scaling_factor,
 )
-import idaes.logger as idaeslogger
-from idaes.core.util.exceptions import InitializationError
+
 from watertap.core.util.model_diagnostics.infeasible import *
-from watertap.unit_models.reverse_osmosis_1D import (
-    ReverseOsmosis1D,
-    ConcentrationPolarizationType,
-    MassTransferCoefficient,
-    PressureChangeType,
-)
-
-from watertap.unit_models.pressure_changer import Pump
 from watertap.core.util.initialization import *
-from idaes.models.unit_models.mixer import (
-    Mixer,
-    MomentumMixingType,
-    MaterialBalanceType,
-)
-from idaes.models.unit_models.separator import (
-    SplittingType,
-    EnergySplittingType,
-)
-from watertap.costing import (
-    WaterTAPCosting,
-    PumpType,
-    MixerType,
-    ROType,
-)
-
-# from analysisWaterTAP.utils.flowsheet_utils import *
-# from watertap.examples.flowsheets.RO_with_energy_recovery.RO_with_energy_recovery import (
-#     calculate_operating_pressure,
-# )
-
-# from analysisWaterTAP.utils import flowsheet_utils as fsTool
-# from analysisWaterTAP.flowsheets.lssro_oaro.costing.LSRRO_ORARO_costing import *
-from idaes.models.unit_models import Product, Feed, StateJunction, Separator
-from idaes.core.util.model_statistics import *
 from watertap.property_models.water_prop_pack import WaterParameterBlock
+from watertap.property_models.unit_specific.cryst_prop_pack import NaClParameterBlock
 
 from watertap_contrib.reflo.costing import TreatmentCosting
-from watertap.property_models.seawater_prop_pack import SeawaterParameterBlock
-from watertap.property_models.unit_specific.cryst_prop_pack import (
-    NaClParameterBlock,
-    NaClStateBlock,
-)
 from watertap_contrib.reflo.unit_models.multi_effect_crystallizer import (
     MultiEffectCrystallizer,
 )
-
 from watertap_contrib.reflo.unit_models.crystallizer_effect import CrystallizerEffect
 
 rho = 1000 * pyunits.kg / pyunits.m**3
@@ -191,6 +141,8 @@ def set_system_operating_conditions(
     m.fs.feed.properties[0].flow_mass_phase_comp["Sol", "NaCl"].fix(0)
     m.fs.feed.properties[0].temperature.fix(298.15)
     m.fs.feed.properties[0].pressure.fix(101325)
+
+    m.fs.feed.properties[0].flow_vol_phase["Liq"]
 
 
 def init_system(m, blk):
