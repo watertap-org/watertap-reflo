64827b61fae25ce4c0334291aa18f61b8dcca5fc
kurbansitterley <kurban.sitterley@nrel.gov>
2025-02-03 12:35:55 -0700

    create initial trough pysam component file

---
+++ src/watertap_contrib/reflo/solar_models/zero_order/trough_pysam.py
@@ -0,0 +1,131 @@
+#################################################################################
+# WaterTAP Copyright (c) 2020-2024, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory, Oak Ridge National Laboratory,
+# National Renewable Energy Laboratory, and National Energy Technology
+# Laboratory (subject to receipt of any required approvals from the U.S. Dept.
+# of Energy). All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and license
+# information, respectively. These files are also available online at the URL
+# "https://github.com/watertap-org/watertap/"
+#################################################################################
+
+import pandas as pd
+from pyomo.environ import (
+    Var,
+    Param,
+    Constraint,
+    Expression,
+    value,
+    check_optimal_termination,
+    units as pyunits,
+)
+from idaes.core import declare_process_block_class
+import idaes.core.util.scaling as iscale
+from idaes.core.util.exceptions import InitializationError
+import idaes.logger as idaeslog
+
+from watertap.core.solvers import get_solver
+from watertap_contrib.reflo.core import SolarEnergyBaseData
+from watertap_contrib.reflo.costing.solar.trough_surrogate import cost_trough_surrogate
+
+__author__ = "Kurban Sitterley"
+
+
+@declare_process_block_class("TroughPySAM")
+class TroughPySAMData(SolarEnergyBaseData):
+    """
+    PySAM model for concentrating solar trough.
+    """
+
+    def build(self):
+        super().build()
+
+        self._tech_type = "trough"
+
+        self.heat_load = Var(
+            initialize=1,
+            bounds=(0, None),
+            units=pyunits.megawatt,
+        )
+
+        self.heat_annual = Var(
+            initialize=1,
+            units=pyunits.kWh,
+        )
+
+        self.electricity_annual = Var(
+            initialize=1,
+            units=pyunits.kWh,
+        )
+
+        self.hours_storage = Param(
+            initialize=1,
+            mutable=True,
+            units=pyunits.hour,
+        )
+
+        self.heat_constraint = Constraint(
+            expr=self.heat_annual
+            == self.heat * pyunits.convert(1 * pyunits.year, to_units=pyunits.hour)
+        )
+
+        self.electricity_constraint = Constraint(
+            expr=self.electricity_annual
+            == self.electricity
+            * pyunits.convert(1 * pyunits.year, to_units=pyunits.hour)
+        )
+
+
+    def calculate_scaling_factors(self):
+
+        if iscale.get_scaling_factor(self.hours_storage) is None:
+            sf = iscale.get_scaling_factor(self.hours_storage, default=1)
+            iscale.set_scaling_factor(self.hours_storage, sf)
+
+        if iscale.get_scaling_factor(self.heat_load) is None:
+            sf = iscale.get_scaling_factor(self.heat_load, default=1, warning=True)
+            iscale.set_scaling_factor(self.heat_load, sf)
+
+        if iscale.get_scaling_factor(self.heat) is None:
+            sf = iscale.get_scaling_factor(self.heat, default=1, warning=True)
+            iscale.set_scaling_factor(self.heat, sf)
+
+        if iscale.get_scaling_factor(self.electricity) is None:
+            sf = iscale.get_scaling_factor(self.electricity, default=1, warning=True)
+            iscale.set_scaling_factor(self.electricity, sf)
+
+    def initialize(
+        self,
+        outlvl=idaeslog.NOTSET,
+        solver=None,
+        optarg=None,
+    ):
+        """
+        General wrapper for initialization routines
+
+        Keyword Arguments:
+            outlvl : sets output level of initialization routine
+            optarg : solver options dictionary object (default=None)
+            solver : str indicating which solver to use during
+                     initialization (default = None)
+
+        Returns: None
+        """
+        init_log = idaeslog.getInitLogger(self.name, outlvl, tag="unit")
+
+        if solver is None:
+            opt = get_solver(optarg)
+
+        res = opt.solve(self)
+
+        init_log.info_high(f"Initialization Step 2 {idaeslog.condition(res)}")
+
+        if not check_optimal_termination(res):
+            raise InitializationError(f"Unit model {self.name} failed to initialize")
+
+        init_log.info("Initialization Complete: {}".format(idaeslog.condition(res)))
+
+    @property
+    def default_costing_method(self):
+        return cost_trough_surrogate
