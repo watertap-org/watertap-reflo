c137bf0ef02688d5a90f3b6f038c5970458c362f
kurbansitterley <kurban.sitterley@nrel.gov>
2025-04-08 10:17:11 -0600

    black on save_water_dams

--- src/watertap_contrib/reflo/analysis/case_studies/KBHDP/utils/case_study_water_dams.py
+++ src/watertap_contrib/reflo/analysis/case_studies/KBHDP/utils/case_study_water_dams.py
@@ -7,20 +7,18 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 
 
 def save_water_dams(
-        df,
-        feed_conditions = None,
-        global_costing_blk="fs.treatment.costing",
-        xcol = None,
-        treatment_unit_costing_dict = None,
-        energy_unit_costing_dict = None,
-        heat_gen_unit_dict = None,
-        elec_gen_unit_dict = None,
-        ):
-    
-
-    '''
+    df,
+    feed_conditions=None,
+    global_costing_blk="fs.treatment.costing",
+    xcol=None,
+    treatment_unit_costing_dict=None,
+    energy_unit_costing_dict=None,
+    heat_gen_unit_dict=None,
+    elec_gen_unit_dict=None,
+):
+    """
     df : Dataframe that saves all relevant costing columns
-    '''
+    """
 
     # Get costing params
     costing_params = dict()
@@ -39,34 +37,50 @@ def save_water_dams(
             raise ValueError(f"Global parameter {gp} does not have uniform value.")
         costing_params[gp] = df[f"{global_costing_blk}.{gp}"].iloc[0]
 
-
     ### Start saving to WaterDAMS
     water_dams_df = pd.DataFrame()
 
     # Add LCOW
-    water_dams_df['LCOW ($/m3)'] = df['fs.costing.LCOT']
+    water_dams_df["LCOW ($/m3)"] = df["fs.costing.LCOT"]
 
-    for u,k in xcol_dict.items():
+    for u, k in xcol_dict.items():
         water_dams_df[u] = df[k]
 
     # Add Treatment heat requirement
-    water_dams_df['Treatment Units Heat Consumed (MWh/year)'] = df['fs.treatment.costing.aggregate_flow_heat']*pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh/pyunits.year)()
+    water_dams_df["Treatment Units Heat Consumed (MWh/year)"] = (
+        df["fs.treatment.costing.aggregate_flow_heat"]
+        * pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh / pyunits.year)()
+    )
     # Add treatment electricity requirement
-    water_dams_df['Treatment Units Electricity Consumed (MWh/year)'] = df['fs.treatment.costing.aggregate_flow_electricity']*pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh/pyunits.year)()
+    water_dams_df["Treatment Units Electricity Consumed (MWh/year)"] = (
+        df["fs.treatment.costing.aggregate_flow_electricity"]
+        * pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh / pyunits.year)()
+    )
 
     # Calculate total heat and electricity generated
-    water_dams_df['Heat Generated (MWh/year)'] = 0 
-    water_dams_df['Electricity Generated (MWh/year)'] = 0 
-    
-    if heat_gen_unit_dict!= None:
+    water_dams_df["Heat Generated (MWh/year)"] = 0
+    water_dams_df["Electricity Generated (MWh/year)"] = 0
+
+    if heat_gen_unit_dict != None:
         # Add heat generating unit electricity requirement
-        for key,value in heat_gen_unit_dict.items():
-            water_dams_df['Heat Units Electricity Consumed (MWh/year)'] = df[value + '.electricity']*pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh/pyunits.year)()
+        for key, value in heat_gen_unit_dict.items():
+            water_dams_df["Heat Units Electricity Consumed (MWh/year)"] = (
+                df[value + ".electricity"]
+                * pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh / pyunits.year)()
+            )
 
-            water_dams_df['Heat Generated (MWh/year)'] = water_dams_df['Heat Generated (MWh/year)'] - df[value + '.heat']*pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh/pyunits.year)()
+            water_dams_df["Heat Generated (MWh/year)"] = (
+                water_dams_df["Heat Generated (MWh/year)"]
+                - df[value + ".heat"]
+                * pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh / pyunits.year)()
+            )
 
-    if elec_gen_unit_dict!= None:
-        water_dams_df['Electricity Generated (MWh/year)'] = water_dams_df['Electricity Generated (MWh/year)'] - df[value + '.electricity']*pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh/pyunits.year)()
+    if elec_gen_unit_dict != None:
+        water_dams_df["Electricity Generated (MWh/year)"] = (
+            water_dams_df["Electricity Generated (MWh/year)"]
+            - df[value + ".electricity"]
+            * pyunits.convert(1 * pyunits.kW, to_units=pyunits.MWh / pyunits.year)()
+        )
 
     # Add Treatment Capex and Opex
     total_treatment_capex = 0
@@ -79,15 +93,14 @@ def save_water_dams(
         try:
             print(f"{b}.capital_cost")
             unit_capex += (
-                df[f"{b}.capital_cost"]
-                * costing_params["total_investment_factor"]
+                df[f"{b}.capital_cost"] * costing_params["total_investment_factor"]
             )  # USD2023
 
         except KeyError:
             print(f"No CAPEX for {u} found.")
             pass
 
-        water_dams_df[u +' Capex ($)'] = unit_capex
+        water_dams_df[u + " Capex ($)"] = unit_capex
 
         total_treatment_capex += unit_capex  # $
 
@@ -109,7 +122,7 @@ def save_water_dams(
             print(f"No Variable OPEX for {u} found.")
             pass
 
-        water_dams_df[u +' Opex ($/year)'] = unit_opex_total
+        water_dams_df[u + " Opex ($/year)"] = unit_opex_total
 
         total_treatment_opex += unit_opex_total  # $ / year
 
@@ -124,15 +137,14 @@ def save_water_dams(
         try:
             print(f"{b}.capital_cost")
             unit_capex += (
-                df[f"{b}.capital_cost"]
-                * costing_params["total_investment_factor"]
+                df[f"{b}.capital_cost"] * costing_params["total_investment_factor"]
             )  # USD2023
 
         except KeyError:
             print(f"No CAPEX for {u} found.")
             pass
 
-        water_dams_df[u +' Capex ($)'] = unit_capex
+        water_dams_df[u + " Capex ($)"] = unit_capex
 
         total_energy_capex += unit_capex  # $
 
@@ -154,84 +166,93 @@ def save_water_dams(
             print(f"No Variable OPEX for {u} found.")
             pass
 
-        water_dams_df[u +' Opex ($/year)'] = unit_opex_total
+        water_dams_df[u + " Opex ($/year)"] = unit_opex_total
 
         total_energy_opex += unit_opex_total  # $ / year
 
-    water_dams_df.insert(1,'Total Capex ($)', total_treatment_capex + total_energy_capex)
-    water_dams_df.insert(2,'Total Opex ($/year)', total_treatment_opex + total_energy_opex)
+    water_dams_df.insert(
+        1, "Total Capex ($)", total_treatment_capex + total_energy_capex
+    )
+    water_dams_df.insert(
+        2, "Total Opex ($/year)", total_treatment_opex + total_energy_opex
+    )
 
-    if feed_conditions!=None:
-        water_dams_df.insert(3,'Feed Flow Rate (kg/s)', df[feed_conditions["feed_flow_mass_water"]] + df[feed_conditions['feed_flow_mass_tds']])
-        water_dams_df.insert(4,'TDS Flow Rate (kg/s)', df[feed_conditions['feed_flow_mass_tds']])
+    if feed_conditions != None:
+        water_dams_df.insert(
+            3,
+            "Feed Flow Rate (kg/s)",
+            df[feed_conditions["feed_flow_mass_water"]]
+            + df[feed_conditions["feed_flow_mass_tds"]],
+        )
+        water_dams_df.insert(
+            4, "TDS Flow Rate (kg/s)", df[feed_conditions["feed_flow_mass_tds"]]
+        )
     else:
-        water_dams_df.insert(3,'Feed Flow Rate (kg/s)', 'Unknown')
-        water_dams_df.insert(4,'TDS Flow Rate (kg/s)', 'Unknown')
+        water_dams_df.insert(3, "Feed Flow Rate (kg/s)", "Unknown")
+        water_dams_df.insert(4, "TDS Flow Rate (kg/s)", "Unknown")
 
     return water_dams_df
-
 
 
 if __name__ == "__main__":
 
     # TODO
     # Flow , salinity
-   
+
     # File path
     filepath = os.path.abspath(__file__)
     util_dir = os.path.dirname(filepath)
     project_folder = os.path.dirname(util_dir)
     water_dams_folder = os.path.join(project_folder, "water_dams")
 
-    test_filename = util_dir + "/test_water_dams.csv" 
-   
+    test_filename = util_dir + "/test_water_dams.csv"
+
     df = pd.read_csv(test_filename).drop(columns="Unnamed: 0")
 
     feed_conditions = {
-        'feed_flow_mass_water': 'fs.treatment.feed.properties[0.0].flow_mass_phase_comp[Liq,H2O]',
-        'feed_flow_mass_tds':'fs.treatment.feed.properties[0.0].flow_mass_phase_comp[Liq,TDS]'
+        "feed_flow_mass_water": "fs.treatment.feed.properties[0.0].flow_mass_phase_comp[Liq,H2O]",
+        "feed_flow_mass_tds": "fs.treatment.feed.properties[0.0].flow_mass_phase_comp[Liq,TDS]",
     }
 
     # Sweep Variables
     xcol_dict = {
-    "Water Recovery": "fs.water_recovery",
-    "Heat Price ($/kWh)": "fs.costing.heat_cost_buy",
-    "Thermal Hours Storage (h)":"fs.energy.FPC.hours_storage",
-    "Fraction of Heat from Grid":"fs.costing.frac_heat_from_grid",
-    "Injecton Cost ($/m3)": "fs.treatment.costing.deep_well_injection.dwi_lcow",
-    "FPC Collector Cost ($/m2)":"fs.energy.costing.flat_plate.cost_per_area_collector",
-    "Thermal Storage Cost ($/m3)":"fs.energy.costing.flat_plate.cost_per_volume_storage",
+        "Water Recovery": "fs.water_recovery",
+        "Heat Price ($/kWh)": "fs.costing.heat_cost_buy",
+        "Thermal Hours Storage (h)": "fs.energy.FPC.hours_storage",
+        "Fraction of Heat from Grid": "fs.costing.frac_heat_from_grid",
+        "Injecton Cost ($/m3)": "fs.treatment.costing.deep_well_injection.dwi_lcow",
+        "FPC Collector Cost ($/m2)": "fs.energy.costing.flat_plate.cost_per_area_collector",
+        "Thermal Storage Cost ($/m3)": "fs.energy.costing.flat_plate.cost_per_volume_storage",
     }
 
     # Costing for each treatment unit model
     treatment_unit_costing_dict = {
-    "MD": "fs.treatment.md.unit",
-    "DWI": "fs.treatment.dwi.unit.costing",
+        "MD": "fs.treatment.md.unit",
+        "DWI": "fs.treatment.dwi.unit.costing",
     }
 
     # Costing for each energy unit model
     energy_unit_costing_dict = {
-        "FPC": "fs.energy.FPC.costing", 
+        "FPC": "fs.energy.FPC.costing",
     }
 
     # Heat generating unit models
     heat_gen_unit_dict = {
-    "FPC": "fs.energy.FPC",
-        }
-    
+        "FPC": "fs.energy.FPC",
+    }
+
     # Electricity generating unit models
     elec_gen_unit_dict = None
 
-
     results = save_water_dams(
-    df=df,
-    feed_conditions = feed_conditions,
-    global_costing_blk="fs.treatment.costing",
-    xcol = xcol_dict,
-    treatment_unit_costing_dict = treatment_unit_costing_dict,
-    energy_unit_costing_dict = energy_unit_costing_dict,
-    heat_gen_unit_dict = heat_gen_unit_dict,
-    elec_gen_unit_dict = elec_gen_unit_dict,
+        df=df,
+        feed_conditions=feed_conditions,
+        global_costing_blk="fs.treatment.costing",
+        xcol=xcol_dict,
+        treatment_unit_costing_dict=treatment_unit_costing_dict,
+        energy_unit_costing_dict=energy_unit_costing_dict,
+        heat_gen_unit_dict=heat_gen_unit_dict,
+        elec_gen_unit_dict=elec_gen_unit_dict,
     )
 
     results.to_csv(os.path.join(water_dams_folder, "test.csv"))
