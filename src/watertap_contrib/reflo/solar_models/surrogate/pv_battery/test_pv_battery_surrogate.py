6b4e1026a7e43411e6212bd4fe7b7806351a865e
kurbansitterley <kurban.sitterley@nrel.gov>
2025-08-17 18:32:51 -0600

    add final pv_batt test file

--- src/watertap_contrib/reflo/solar_models/surrogate/pv_battery/test_pv_battery_surrogate.py
+++ src/watertap_contrib/reflo/solar_models/surrogate/pv_battery/test_pv_battery_surrogate.py
@@ -40,25 +40,31 @@ from idaes.core.util.scaling import (
 from watertap.core.solvers import get_solver
 from watertap_contrib.reflo.costing import EnergyCosting
 from watertap_contrib.reflo.solar_models import (
-    PVSurrogate,
-    generate_pv_data,
+    PVBatterySurrogate,
+    generate_pv_battery_data,
 )
 
 solver = get_solver()
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-dataset_filename = os.path.join(os.path.dirname(__file__), "data/test_pv_data.pkl")
+dataset_filename = os.path.join(
+    os.path.dirname(__file__), "data/test_pv_battery_data.pkl"
+)
 surrogate_model_file = os.path.join(
-    os.path.dirname(__file__), "data/test_pv_surrogate.json"
+    os.path.dirname(__file__), "data/test_pv_battery_surrogate.json"
 )
 
 
-def build_pv():
+def build_pv_battery():
 
-    pv_dict = {
+    pv_batt_dict = {
         "input_variables": {
-            "labels": ["system_capacity"],
-            "units": {"system_capacity": "kW"},
+            "labels": ["system_capacity", "battery_power", "hours_storage"],
+            "units": {
+                "system_capacity": "kW",
+                "battery_power": "kW",
+                "hours_storage": "hours",
+            },
         },
         "output_variables": {
             "labels": ["electricity_annual", "land_req"],
@@ -71,26 +77,26 @@ def build_pv():
 
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.pv = PVSurrogate(**pv_dict)
+    m.fs.pv_batt = PVBatterySurrogate(**pv_batt_dict)
 
     return m
 
 
-class TestPV:
+class TestPVBattery:
     @pytest.fixture(scope="class")
-    def pv_frame(self):
-        return build_pv()
+    def pv_batt_frame(self):
+        return build_pv_battery()
 
     @pytest.mark.unit
-    def test_build(self, pv_frame):
-        pv = pv_frame.fs.pv
+    def test_build(self, pv_batt_frame):
+        pv = pv_batt_frame.fs.pv_batt
 
         assert len(pv.config) == 15
         assert not pv.config.dynamic
         assert not pv.config.has_holdup
         assert pv.config.training_fraction == 0.8
         assert not pv.config.scale_training_data
-        assert isinstance(pv, PVSurrogate)
+        assert isinstance(pv, PVBatterySurrogate)
         assert isinstance(pv.surrogate_blk, SurrogateBlock)
         assert hasattr(pv, "data")
         assert hasattr(pv, "data_training")
@@ -127,7 +133,7 @@ class TestPV:
                 no_ports.append(c)
 
         assert len(no_ports) == 0
-        assert number_variables(pv) == 4
+        assert number_variables(pv) == 6
         assert number_unused_variables(pv) == 0
         assert number_total_constraints(pv) == 3
         assert not hasattr(pv, "heat")
@@ -135,67 +141,75 @@ class TestPV:
         assert isinstance(pv.electricity_constraint, Constraint)
 
     @pytest.mark.unit
-    def test_surrogate_metrics(self, pv_frame):
-        pv = pv_frame.fs.pv
+    def test_surrogate_metrics(self, pv_batt_frame):
+        pv = pv_batt_frame.fs.pv_batt
 
         metrics = pv.compute_fit_metrics()
         for output_label in pv.output_labels:
             assert metrics[output_label]["R2"] > 0.99
 
     @pytest.mark.unit
-    def test_dof(self, pv_frame):
-        m = pv_frame
-        pv = m.fs.pv
-        assert degrees_of_freedom(m) == 1
-        pv.system_capacity.fix(500000)
+    def test_dof(self, pv_batt_frame):
+        m = pv_batt_frame
+        pv = pv_batt_frame.fs.pv_batt
+        assert degrees_of_freedom(m) == 3
+        pv.system_capacity.fix(250000)
+        pv.battery_power.fix(10000)
+        pv.hours_storage.fix(12)
         assert degrees_of_freedom(m) == 0
 
     @pytest.mark.unit
-    def test_calculate_scaling(self, pv_frame):
-        m = pv_frame
+    def test_calculate_scaling(self, pv_batt_frame):
+        m = pv_batt_frame
         calculate_scaling_factors(m)
         assert len(list(unscaled_variables_generator(m))) == 0
 
     @pytest.mark.component
-    def test_initialization(self, pv_frame):
-        m = pv_frame
-        initialization_tester(m, unit=m.fs.pv)
+    def test_initialization(self, pv_batt_frame):
+        m = pv_batt_frame
+        initialization_tester(m, unit=m.fs.pv_batt)
 
     @pytest.mark.component
-    def test_solvability(self, pv_frame):
-        m = pv_frame
-        pv = m.fs.pv
+    def test_solvability(self, pv_batt_frame):
+        m = pv_batt_frame
+        pv = m.fs.pv_batt
         for i, row in pv.data.iterrows():
             pv.system_capacity.fix(row["system_capacity"])
+            pv.battery_power.fix(row["battery_power"])
+            pv.hours_storage.fix(row["hours_storage"])
             results = solver.solve(m)
             assert_optimal_termination(results)
 
     @pytest.mark.component
-    def test_solution(self, pv_frame):
-        m = pv_frame
-        pv = m.fs.pv
-        pv.system_capacity.fix(500000)
+    def test_solution(self, pv_batt_frame):
+        m = pv_batt_frame
+        pv = m.fs.pv_batt
+        pv.system_capacity.fix(250000)
+        pv.battery_power.fix(10000)
+        pv.hours_storage.fix(12)
 
         results = solver.solve(m)
         assert_optimal_termination(results)
 
-        pv_unit_results = {
-            "electricity": 143093.81,  # kW
-            "electricity_annual": 1254360357.31,  # kWh/year
-            "land_req": 2002.39,  # acre
-            "inverter_capacity": 416666.66,  # kW
+        pv_batt_unit_results = {
+            "electricity": 69386.78,
+            "electricity_annual": 608244546.34,
+            "land_req": 1001.12,
+            "inverter_capacity": 208333.33,
         }
-        for v, r in pv_unit_results.items():
+        for v, r in pv_batt_unit_results.items():
             mv = pv.find_component(v)
             assert pytest.approx(value(mv), rel=1e-3) == r
 
     @pytest.mark.component
-    def test_costing_simple(self, pv_frame):
-        m = pv_frame
-        pv = m.fs.pv
+    def test_costing(self, pv_batt_frame):
+        m = pv_batt_frame
+        pv = m.fs.pv_batt
 
         m.fs.costing = EnergyCosting()
-        m.fs.pv.costing = UnitModelCostingBlock(flowsheet_costing_block=m.fs.costing)
+        m.fs.pv_batt.costing = UnitModelCostingBlock(
+            flowsheet_costing_block=m.fs.costing
+        )
         m.fs.costing.electricity_cost.fix(0.0)
         m.fs.costing.land_cost.set_value(10000)
         m.fs.costing.cost_process()
@@ -206,75 +220,16 @@ class TestPV:
         assert_optimal_termination(results)
 
         sys_costing_results = {
-            "aggregate_capital_cost": 820023903.94,
-            "aggregate_fixed_operating_cost": 15500000.0,
-            "aggregate_variable_operating_cost": 0.0,
-            "aggregate_flow_electricity": -143093.81,
-            "aggregate_flow_costs": {"electricity": 0.0},
-            "aggregate_direct_capital_cost": 820023903.94,
-            "total_capital_cost": 820023903.94,
-            "total_operating_cost": 40100717.11,
-            "maintenance_labor_chemical_operating_cost": 24600717.11,
-            "total_fixed_operating_cost": 40100717.11,
-            "total_variable_operating_cost": 0.0,
-            "total_annualized_cost": 131907271.65,
-            "LCOE": 0.105251,
-        }
-        for v, r in sys_costing_results.items():
-            cv = m.fs.costing.find_component(v)
-            if cv.is_indexed():
-                for i, rr in r.items():
-                    assert pytest.approx(value(cv[i]), rel=1e-3) == rr
-            else:
-                assert pytest.approx(value(cv), rel=1e-3) == r
-
-        pv_costing_results = {
-            "capital_cost": 820023903.94,
-            "direct_capital_cost": 820023903.94,
-            "fixed_operating_cost": 15500000.0,
-            "direct_cost": 800000000.0,
-            "land_cost": 20023903.94,
-        }
-
-        for v, r in pv_costing_results.items():
-            mv = pv.costing.find_component(v)
-            assert pytest.approx(value(mv), rel=1e-3) == r
-
-    @pytest.mark.component
-    def test_costing_detailed(self):
-        m = build_pv()
-        pv = m.fs.pv
-        pv.system_capacity.fix(500000)
-        calculate_scaling_factors(m)
-        initialization_tester(m, unit=pv)
-        results = solver.solve(m)
-        assert_optimal_termination(results)
-        m.fs.costing = EnergyCosting()
-        m.fs.pv.costing = UnitModelCostingBlock(
-            flowsheet_costing_block=m.fs.costing,
-            costing_method_arguments={"cost_method": "detailed"},
-        )
-        m.fs.costing.electricity_cost.fix(0)
-        m.fs.costing.land_cost.set_value(10000)
-        m.fs.costing.cost_process()
-        m.fs.costing.add_LCOE()
-        m.fs.costing.initialize()
-
-        results = solver.solve(m)
-        assert_optimal_termination(results)
-
-        sys_costing_results = {
-            "aggregate_capital_cost": 572322807.89,
-            "aggregate_fixed_operating_cost": 15500000.0,
-            "aggregate_flow_electricity": -143093.81,
-            "aggregate_flow_costs": {"electricity": 0.0},
-            "aggregate_direct_capital_cost": 572322807.89,
-            "total_capital_cost": 572322807.89,
-            "total_operating_cost": 32669684.23,
-            "maintenance_labor_chemical_operating_cost": 17169684.23,
-            "total_fixed_operating_cost": 32669684.23,
-            "total_annualized_cost": 96744627.44,
-            "LCOE": 0.077194,
+            "aggregate_capital_cost": 309695810.29,
+            "aggregate_fixed_operating_cost": 10132000.0,
+            "aggregate_flow_electricity": -69386.78,
+            "aggregate_direct_capital_cost": 309695810.29,
+            "total_capital_cost": 309695810.29,
+            "total_operating_cost": 19422874.3,
+            "maintenance_labor_chemical_operating_cost": 9290874.3,
+            "total_fixed_operating_cost": 19422874.3,
+            "total_annualized_cost": 54095162.71,
+            "LCOE": 0.089014,
         }
 
         for v, r in sys_costing_results.items():
@@ -286,13 +241,17 @@ class TestPV:
                 assert pytest.approx(value(cv), rel=1e-3) == r
 
         pv_costing_results = {
-            "capital_cost": 572322807.89,
-            "direct_capital_cost": 572322807.89,
-            "fixed_operating_cost": 15500000.0,
-            "direct_cost": 507275000.0,
-            "indirect_cost": 45023903.94,
-            "land_cost": 20023903.94,
-            "total_installed_cost_per_capacity": 1.1446,
+            "capital_cost": 309695810.29,
+            "direct_capital_cost": 309695810.29,
+            "fixed_operating_cost": 10132000.0,
+            "direct_cost": 287184600.0,
+            "indirect_cost": 22511210.29,
+            "land_cost": 10011210.29,
+            "pv_direct_capital_cost": 246250000.0,
+            "battery_direct_capital_cost": 32570000.0,
+            "pv_fixed_operating_cost": 7750000.0,
+            "battery_fixed_operating_cost": 870000.0,
+            "battery_replacement_cost": 1512000.0,
         }
 
         for v, r in pv_costing_results.items():
@@ -300,12 +259,12 @@ class TestPV:
             assert pytest.approx(value(mv), rel=1e-3) == r
 
 
-class TestCreatePVSurrogate:
+class TestCreatePVBatterySurrogate:
 
     @pytest.mark.component
-    def test_run_pysam_pv(self):
+    def test_run_pysam_pv_battery(self):
 
-        test_df = generate_pv_data()
+        test_df = generate_pv_battery_data()
 
         assert all(x > 0 for x in test_df.electricity_annual.to_list())
         assert all(x > 0 for x in test_df.land_req.to_list())
@@ -314,10 +273,14 @@ class TestCreatePVSurrogate:
     def test_create_with_scaling_data(self):
 
         test_dataset_filename = os.path.join(__location__, "data/test_data.pkl")
-        pv_dict = {
+        pv_batt_dict = {
             "input_variables": {
-                "labels": ["system_capacity"],
-                "units": {"system_capacity": "kW"},
+                "labels": ["system_capacity", "battery_power", "hours_storage"],
+                "units": {
+                    "system_capacity": "kW",
+                    "battery_power": "kW",
+                    "hours_storage": "h",
+                },
             },
             "output_variables": {
                 "labels": ["electricity_annual", "land_req"],
@@ -329,45 +292,14 @@ class TestCreatePVSurrogate:
 
         m = ConcreteModel()
         m.fs = FlowsheetBlock(dynamic=False)
-        m.fs.pv = PVSurrogate(**pv_dict)
-        m.fs.pv.system_capacity.fix(5000)
+        m.fs.pv_batt = PVBatterySurrogate(**pv_batt_dict)
+        m.fs.pv_batt.system_capacity.fix(200000)
+        m.fs.pv_batt.battery_power.fix(50000)
+        m.fs.pv_batt.hours_storage.fix(12)
         calculate_scaling_factors(m)
-        initialization_tester(m, unit=m.fs.pv)
-        results = solver.solve(m)
-        assert_optimal_termination(results)
-
-    @pytest.mark.component
-    def test_create_polynomial_surrogate(self):
-
-        test_dataset_filename = os.path.join(__location__, "data/test_data.pkl")
-        dataset_filename = os.path.join(
-            os.path.dirname(__file__), "data/test_pv_data.pkl"
-        )
-        pv_dict = {
-            "input_variables": {
-                "labels": ["system_capacity"],
-                "units": {"system_capacity": "kW"},
-            },
-            "output_variables": {
-                "labels": ["electricity_annual", "land_req"],
-                "units": {"electricity_annual": "kWh/year", "land_req": "acre"},
-            },
-            "scale_training_data": True,
-            "dataset_filename": dataset_filename,
-            "surrogate_filename_save": dataset_filename.replace(".pkl", "_poly.json"),
-            "maximum_polynomial_order": 1,
-            "surrogate_model_type": "polynomial",
-        }
-
-        m = ConcreteModel()
-        m.fs = FlowsheetBlock(dynamic=False)
-        m.fs.pv = PVSurrogate(**pv_dict)
-        m.fs.pv.system_capacity.fix(500000)
-        calculate_scaling_factors(m)
-        initialization_tester(m, unit=m.fs.pv)
+        initialization_tester(m, unit=m.fs.pv_batt)
         results = solver.solve(m)
         assert_optimal_termination(results)
 
         os.remove(test_dataset_filename)
         os.remove(test_dataset_filename.replace(".pkl", ".json"))
-        os.remove(dataset_filename.replace(".pkl", "_poly.json"))
