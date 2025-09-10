d86fd53f4fed3df4a606aa2885e84bea598ed1b2
kurbansitterley <kurban.sitterley@nrel.gov>
2025-09-10 15:09:29 -0600

    clean up UF

--- src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/UF.py
+++ src/watertap_contrib/reflo/analysis/case_studies/KBHDP/components/UF.py
@@ -1,50 +1,24 @@
-import os
-import math
-import numpy as np
 from pyomo.environ import (
     ConcreteModel,
-    value,
-    Param,
-    Var,
-    Constraint,
-    Set,
-    Expression,
     TransformationFactory,
-    Objective,
-    NonNegativeReals,
-    Block,
-    RangeSet,
-    check_optimal_termination,
+    assert_optimal_termination,
+    value,
     units as pyunits,
 )
-from pyomo.network import Arc, SequentialDecomposition
-from pyomo.util.check_units import assert_units_consistent
-from idaes.core import FlowsheetBlock, UnitModelCostingBlock, MaterialFlowBasis
-from idaes.core.solvers import get_solver
-from idaes.core.util.initialization import propagate_state as _prop_state
+from pyomo.network import Arc
+
 import idaes.core.util.scaling as iscale
-from idaes.core.util.scaling import (
-    constraint_scaling_transform,
-    calculate_scaling_factors,
-    set_scaling_factor,
-)
-import idaes.logger as idaeslogger
-from idaes.core.util.exceptions import InitializationError
-from idaes.models.unit_models import Product, Feed, StateJunction, Separator
-from idaes.core.util.model_statistics import *
-from watertap.core.util.initialization import *
-from watertap.core.util.model_diagnostics.infeasible import *
-from watertap.property_models.NaCl_prop_pack import NaClParameterBlock
-from watertap.property_models.multicomp_aq_sol_prop_pack import MCASParameterBlock
+from idaes.core import FlowsheetBlock, UnitModelCostingBlock
+from idaes.core.util.initialization import propagate_state
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.models.unit_models import StateJunction
+
+from watertap.core.wt_database import Database
 from watertap.core.zero_order_properties import WaterParameterBlock
 from watertap.unit_models.zero_order.ultra_filtration_zo import UltraFiltrationZO
-from watertap_contrib.reflo.costing import (
-    TreatmentCosting,
-    EnergyCosting,
-    REFLOCosting,
-)
-from watertap.costing.zero_order_costing import ZeroOrderCosting
-from watertap.core.wt_database import Database
+
+from watertap_contrib.reflo.costing import TreatmentCosting
+from watertap_contrib.reflo.analysis.case_studies.KBHDP.utils import solve
 
 __all__ = [
     "build_UF",
@@ -57,17 +31,12 @@ __all__ = [
 ]
 
 
-def propagate_state(arc):
-    _prop_state(arc)
-    # print(f"Propogation of {arc.source.name} to {arc.destination.name} successful.")
-    # arc.source.display()
-    # print(arc.destination.name)
-    # arc.destination.display()
-    # print('\n')
+def build_UF(blk, prop_package=None):
 
-
-def build_UF(m, blk, prop_package) -> None:
     print(f'\n{"=======> BUILDING ULTRAFILTRATION SYSTEM <=======":^60}\n')
+    m = blk.model()
+    if prop_package is None:
+        prop_package = m.fs.UF_properties
 
     blk.feed = StateJunction(property_package=prop_package)
     blk.product = StateJunction(property_package=prop_package)
@@ -90,29 +59,25 @@ def build_UF(m, blk, prop_package) -> None:
     )
 
 
-def init_UF(m, blk, verbose=True, solver=None):
-    if solver is None:
-        solver = get_solver()
+def init_UF(blk):
 
-    optarg = solver.options
     print(
         "\n\n-------------------- INITIALIZING ULTRAFILTRATION --------------------\n\n"
     )
-    # print(f"System Degrees of Freedom: {degrees_of_freedom(m)}")
     print(f"UF Degrees of Freedom: {degrees_of_freedom(blk)}")
-    print("\n\n")
-    # assert_no_degrees_of_freedom(m)
-    blk.feed.initialize(optarg=optarg)
+
+    blk.feed.initialize()
     propagate_state(blk.feed_to_unit)
-    blk.unit.initialize(optarg=optarg)
+
+    blk.unit.initialize()
     propagate_state(blk.unit_to_disposal)
     propagate_state(blk.unit_to_product)
-    blk.product.initialize(optarg=optarg)
-    blk.disposal.initialize(optarg=optarg)
+
+    blk.product.initialize()
+    blk.disposal.initialize()
 
 
 def set_UF_op_conditions(blk):
-    # blk.feed.properties[0.0].flow_mass_comp["tss"].fix(5.22e-6)
     print(f"UF Degrees of Freedom: {degrees_of_freedom(blk)}")
     blk.unit.recovery_frac_mass_H2O.fix(0.99)
     blk.unit.removal_frac_mass_comp[0, "tds"].fix(1e-3)
@@ -126,117 +91,39 @@ def set_system_conditions(blk):
     blk.feed.properties[0.0].flow_mass_comp["tss"].fix(5.22e-6)
 
 
-def add_UF_costing(m, blk, costing_blk=None):
+def add_UF_costing(blk, costing_blk=None):
     if costing_blk is None:
+        m = blk.model()
         costing_blk = m.fs.costing
 
     blk.unit.costing = UnitModelCostingBlock(
         flowsheet_costing_block=costing_blk,
     )
 
-    # m.fs.costing.cost_process()
-
 
 def add_UF_scaling(blk):
-    # set_scaling_factor(blk.feed.properties[0.0].flow_mass_comp["H2O"], -2)
-    # set_scaling_factor(blk.feed.properties[0.0].flow_mass_comp["tds"], 1)
-    # set_scaling_factor(blk.product.properties[0.0].flow_mass_comp["H2O"], -2)
-    # set_scaling_factor(blk.product.properties[0.0].flow_mass_comp["tds"], 1)
-    set_scaling_factor(blk.disposal.properties[0.0].flow_mass_comp["tds"], 1e3)
-    # set_scaling_factor(blk.unit.properties_in[0.0].flow_mass_comp["H2O"], -2)
-    set_scaling_factor(blk.unit.properties_byproduct[0.0].flow_mass_comp["tds"], 1e3)
-
-
-def load_parameters(m, blk):
-    m.db.get_unit_operation_parameters("ultra_filtration")
-    blk.unit.load_parameters_from_database()
+    iscale.set_scaling_factor(blk.disposal.properties[0.0].flow_mass_comp["tds"], 1e3)
+    iscale.set_scaling_factor(
+        blk.unit.properties_byproduct[0.0].flow_mass_comp["tds"], 1e3
+    )
 
 
 def build_system():
     m = ConcreteModel()
     m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.costing = REFLOCosting()
-    # m.fs.costing = ZeroOrderCosting()
+    m.fs.costing = TreatmentCosting()
     m.db = Database()
-    m.fs.RO_properties = NaClParameterBlock()
-    m.fs.MCAS_properties = MCASParameterBlock(
-        solute_list=[
-            "Alkalinity_2-",
-            "Ca_2+",
-            "Cl_-",
-            "Mg_2+",
-            "K_+",
-            "SiO2",
-            "Na_+",
-            "SO2_-4+",
-        ],
-        material_flow_basis=MaterialFlowBasis.mass,
-    )
     m.fs.params = WaterParameterBlock(solute_list=["tds", "tss"])
 
     m.fs.UF = FlowsheetBlock(dynamic=False)
-    build_UF(m, m.fs.UF, m.fs.params)
+    build_UF(m.fs.UF, prop_package=m.fs.params)
 
     TransformationFactory("network.expand_arcs").apply_to(m)
 
     return m
 
 
-def solve(model, solver=None, tee=True, raise_on_failure=True):
-    # ---solving---
-    if solver is None:
-        solver = get_solver()
-
-    print("\n--------- SOLVING ---------\n")
-
-    results = solver.solve(model, tee=tee)
-
-    if check_optimal_termination(results):
-        print("\n--------- OPTIMAL SOLVE!!! ---------\n")
-        return results
-    msg = (
-        "The current configuration is infeasible. Please adjust the decision variables."
-    )
-    if raise_on_failure:
-        raise RuntimeError(msg)
-    else:
-        return results
-
-
-def print_stream_table(blk):
-    print(f'{"FEED":<20s}')
-    print(
-        f'{"    H2O":<20s}{m.fs.UF.feed.properties[0.0].flow_mass_comp["H2O"].value:<10.3f}{pyunits.get_units(m.fs.UF.feed.properties[0.0].flow_mass_comp["H2O"])}'
-    )
-    print(
-        f'{"    TDS":<20s}{m.fs.UF.feed.properties[0.0].flow_mass_comp["tds"].value:<10.3f}{pyunits.get_units(m.fs.UF.feed.properties[0.0].flow_mass_comp["tds"])}'
-    )
-    print(
-        f'{"    TSS":<20s}{m.fs.UF.feed.properties[0.0].flow_mass_comp["tss"].value:<10.3f}{pyunits.get_units(m.fs.UF.feed.properties[0.0].flow_mass_comp["tss"])}'
-    )
-    print(f'{"PRODUCT":<20s}')
-    print(
-        f'{"    H2O":<20s}{m.fs.UF.product.properties[0.0].flow_mass_comp["H2O"].value:<10.3f}{pyunits.get_units(m.fs.UF.product.properties[0.0].flow_mass_comp["H2O"])}'
-    )
-    print(
-        f'{"    TDS":<20s}{m.fs.UF.product.properties[0.0].flow_mass_comp["tds"].value:<10.3f}{pyunits.get_units(m.fs.UF.product.properties[0.0].flow_mass_comp["tds"])}'
-    )
-    print(
-        f'{"    TSS":<20s}{m.fs.UF.product.properties[0.0].flow_mass_comp["tss"].value:<10.3f}{pyunits.get_units(m.fs.UF.product.properties[0.0].flow_mass_comp["tss"])}'
-    )
-    print(f'{"DISPOSAL":<20s}')
-    print(
-        f'{"    H2O":<20s}{m.fs.UF.disposal.properties[0.0].flow_mass_comp["H2O"].value:<10.3f}{pyunits.get_units(m.fs.UF.disposal.properties[0.0].flow_mass_comp["H2O"])}'
-    )
-    print(
-        f'{"    TDS":<20s}{m.fs.UF.disposal.properties[0.0].flow_mass_comp["tds"].value:<10.3f}{pyunits.get_units(m.fs.UF.disposal.properties[0.0].flow_mass_comp["tds"])}'
-    )
-    print(
-        f'{"    TSS":<20s}{m.fs.UF.disposal.properties[0.0].flow_mass_comp["tss"].value:<10.3f}{pyunits.get_units(m.fs.UF.disposal.properties[0.0].flow_mass_comp["tss"])}'
-    )
-
-
-def report_UF(m, blk, stream_table=False):
+def report_UF(blk):
     print(f"\n\n-------------------- UF Report --------------------\n")
     print("\n")
     print(
@@ -260,66 +147,28 @@ def report_UF(m, blk, stream_table=False):
     )
 
 
-def print_UF_costing_breakdown(blk, debug=False):
-    print(f"\n\n-------------------- Ud Costing Breakdown --------------------\n")
+def print_UF_costing_breakdown(
+    blk,
+):
+    print(f"\n\n-------------------- UF Costing Breakdown --------------------\n")
     print(f'{"UF Capital Cost":<35s}{f"${blk.unit.costing.capital_cost():<25,.0f}"}')
 
-    if debug:
-        print(blk.unit.costing.display())
 
+def main():
 
-def breakdown_dof(blk):
-    equalities = [c for c in activated_equalities_generator(blk)]
-    active_vars = variables_in_activated_equalities_set(blk)
-    fixed_active_vars = fixed_variables_in_activated_equalities_set(blk)
-    unfixed_active_vars = unfixed_variables_in_activated_equalities_set(blk)
-    print("\n ===============DOF Breakdown================\n")
-    print(f"Degrees of Freedom: {degrees_of_freedom(blk)}")
-    print(f"Activated Variables: ({len(active_vars)})")
-    for v in active_vars:
-        print(f"   {v}")
-    print(f"Activated Equalities: ({len(equalities)})")
-    for c in equalities:
-        print(f"   {c}")
-
-    print(f"Fixed Active Vars: ({len(fixed_active_vars)})")
-    for v in fixed_active_vars:
-        print(f"   {v}")
-
-    print(f"Unfixed Active Vars: ({len(unfixed_active_vars)})")
-    for v in unfixed_active_vars:
-        print(f"   {v}")
-    print("\n")
-    print(f" {f' Active Vars':<30s}{len(active_vars)}")
-    print(f"{'-'}{f' Fixed Active Vars':<30s}{len(fixed_active_vars)}")
-    print(f"{'-'}{f' Activated Equalities':<30s}{len(equalities)}")
-    print(f"{'='}{f' Degrees of Freedom':<30s}{degrees_of_freedom(blk)}")
-    print("\nSuggested Variables to Fix:")
-
-    if degrees_of_freedom != 0:
-        unfixed_vars_without_constraint = [
-            v for v in active_vars if v not in unfixed_active_vars
-        ]
-        for v in unfixed_vars_without_constraint:
-            if v.fixed is False:
-                print(f"   {v}")
-
-
-if __name__ == "__main__":
-    file_dir = os.path.dirname(os.path.abspath(__file__))
     m = build_system()
     set_UF_op_conditions(m.fs.UF)
     set_system_conditions(m.fs.UF)
-    # load_parameters(m, m.fs.UF)
-    add_UF_costing(m, m.fs.UF)
+    add_UF_costing(m.fs.UF)
     m.fs.costing.cost_process()
-    # m.fs.costing.initialize()
-    init_UF(m, m.fs.UF)
-    solve(m)
+    init_UF(m.fs.UF)
 
-    report_UF(m, m.fs.UF)
-    m.fs.UF.unit.costing.display()
-    m.fs.costing.display()
-    print_UF_costing_breakdown(m.fs.UF, debug=False)
-    # # print(f"System Degrees of Freedom: {degrees_of_freedom(m)}")
-    print_stream_table(m.fs.UF)
+    results = solve(m)
+    assert_optimal_termination(results)
+
+    report_UF(m.fs.UF)
+    print_UF_costing_breakdown(m.fs.UF)
+
+
+if __name__ == "__main__":
+    main()
